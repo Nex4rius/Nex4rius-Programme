0a335b21945c176334f7996d12b6cd65588a8187
DarknessShadow <DarknessShadow@web.de>
2016-05-01 13:26:50 +0200

    Update autorun.lua

--- autorun.lua
+++ autorun.lua
@@ -1,2 +1,5 @@
-os.execute"wget -f '' control.lua")
-os.execute"wget -f '' control.lua")
+os.execute"wget -f 'https://raw.githubusercontent.com/DarknessShadow/Stargate-Programm/master/control.lua' control.lua")
+os.execute"wget -f 'https://raw.githubusercontent.com/DarknessShadow/Stargate-Programm/master/compat.lua' compat.lua")
+os.execute"wget -f 'https://raw.githubusercontent.com/DarknessShadow/Stargate-Programm/master/config.lua' config.lua")
+os.execute"wget 'https://raw.githubusercontent.com/DarknessShadow/Stargate-Programm/master/addresses.lua' addresses.lua.lua")
+dofile("control.lua")
