4d5d6d421eb24ad52ddf038a6745773464980a3f
DarknessShadow <DarknessShadow@web.de>
2016-05-03 18:35:35 +0200

    Update reset.lua

--- reset.lua
+++ reset.lua
@@ -1,7 +1,6 @@
 os.execute("del -v addresses.lua")
 os.execute("del -v config.lua")
 os.execute("del -v control.lua")
-os.execute("del -v autorun.lua")
 os.execute("del -v saveAfterReboot.lua")
 os.execute("del -v compat.lua")
 os.execute("wget -f 'https://raw.githubusercontent.com/DarknessShadow/Stargate-Programm/test/reset.lua' reset.lua")
