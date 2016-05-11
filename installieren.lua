f6295ebac371f7956c95c12860984aa76cfe3e30
DarknessShadow <DarknessShadow@web.de>
2016-05-11 21:13:14 +0200

    Update installieren.lua

--- installieren.lua
+++ installieren.lua
@@ -1,20 +1,23 @@
 fs = require("filesystem")
 fs.makeDirectory("/stargate")
-os.execute("wget -f 'https://raw.githubusercontent.com/DarknessShadow/Stargate-Programm/master/autorun.lua' autorun.lua")
+serverAddresse = "https://raw.githubusercontent.com/DarknessShadow/Stargate-Programm/"
+versionTyp = "master/"
+Pfad = serverAddresse .. versionTyp
+os.execute("wget -f " .. Pfad .. "autorun.lua' autorun.lua")
 print("")
-os.execute("wget -f 'https://raw.githubusercontent.com/DarknessShadow/Stargate-Programm/master/stargate/check.lua' stargate/check.lua")
+os.execute("wget -f " .. Pfad .. "stargate/check.lua' stargate/check.lua")
 print("")
-os.execute("wget -f 'https://raw.githubusercontent.com/DarknessShadow/Stargate-Programm/master/stargate/control.lua' stargate/control.lua")
+os.execute("wget -f " .. Pfad .. "stargate/Kontrollprogramm.lua' stargate/Kontrollprogramm.lua")
 print("")
-os.execute("wget -f 'https://raw.githubusercontent.com/DarknessShadow/Stargate-Programm/master/stargate/compat.lua' stargate/compat.lua")
+os.execute("wget -f " .. Pfad .. "stargate/compat.lua' stargate/compat.lua")
 print("")
-os.execute("wget -f 'https://raw.githubusercontent.com/DarknessShadow/Stargate-Programm/master/stargate/config.lua' stargate/config.lua")
+os.execute("wget -f " .. Pfad .. "stargate/config.lua' stargate/config.lua")
 print("")
-os.execute("wget 'https://raw.githubusercontent.com/DarknessShadow/Stargate-Programm/master/stargate/addresses.lua' stargate/addresses.lua")
+os.execute("wget " .. Pfad .. "stargate/addressen.lua' stargate/addressen.lua")
 print("")
-os.execute("wget 'https://raw.githubusercontent.com/DarknessShadow/Stargate-Programm/master/stargate/saveAfterReboot.lua' stargate/saveAfterReboot.lua")
+os.execute("wget " .. Pfad .. "stargate/sicherNachNeustart.lua' stargate/sicherNachNeustart.lua")
 print("")
-os.execute("del -v this.lua")
+os.execute("del -v installieren.lua")
 print("\n")
-install = true
+installieren = true
 os.execute("autorun.lua")
