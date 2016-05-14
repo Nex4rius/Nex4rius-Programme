f52373202b0593311c8946594f11ee49fb127fc0
DarknessShadow <DarknessShadow@web.de>
2016-05-14 11:31:42 +0200

    Update chunkloader.lua

--- chunkloader.lua
+++ chunkloader.lua
@@ -1,22 +1,23 @@
--- Robot programm to load the chunk on active redstone signal (white signal from stargate programm)
+-- Robot programm to load the chunk on active redstone signal (eg. white signal from stargate programm)
 
 serverAddresse = "https://raw.githubusercontent.com/DarknessShadow/Stargate-Programm/"
 versionTyp = "master/"
 
 Pfad = serverAddresse .. versionTyp
 os.execute("wget -f " .. Pfad .. "chunkloader.lua chunkloader.lua")
+os.execute("wget -f " .. Pfad .. "stargate/sprache.lua sprache.lua")
 
 Sprache = "deutsch"
-dofile("stargate/sprache.lua")
+dofile("sprache.lua")
 
 print(pruefeKomponenten)
 if component.isAvailable("redstone") then
   print(redstoneOK)
   r = component.getPrimary("redstone")
   aktiv = true
-  while aktiv true do
+--  while aktiv true do
     
-  end
+--  end
 else
   print(redstoneFehlt)
 end
