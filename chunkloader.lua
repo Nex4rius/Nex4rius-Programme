a3aaa9fe9a0538866e0de10b510b13ab465536f4
DarknessShadow <DarknessShadow@web.de>
2016-05-14 14:35:55 +0200

    Update chunkloader.lua

--- chunkloader.lua
+++ chunkloader.lua
@@ -29,14 +29,17 @@ end
 function loop()
   aktiv = true
   while aktiv == true do
-    print("\nredstone input: " .. r.getInput(1))
+    print("\nredstone input: " .. r.getInput(1)) --test
     if r.getInput(1) > 0 then
       c.setActive(true)
-      print("Chunkloader" .. c.isActive())
     elseif r.getInput(1) == 0 then
       os.sleep(10)
       c.setActive(false)
-      print("Chunkloader" .. c.isActive())
+    end
+    if c.isActive() == true then
+      print("Chunkloader An")
+    elseif c.isActive() == false then
+      print("Chunkloader Aus")
     end
     os.sleep(10)
   end
