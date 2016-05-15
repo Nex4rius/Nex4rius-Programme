b2ffaece3b2091213919d51239f2acf55daf61b0
DarknessShadow <DarknessShadow@web.de>
2016-05-15 09:38:31 +0200

    Update check.lua

--- stargate/check.lua
+++ stargate/check.lua
@@ -1,4 +1,4 @@
-version = "1.6.4"
+version = "1.6.5"
 component = require("component")
 sides = require("sides")
 term = require("term")
@@ -75,8 +75,8 @@ function update()
   os.execute("wget -f " .. Pfad .. "stargate/compat.lua stargate/compat.lua") print("")
   os.execute("wget -f " .. Pfad .. "stargate/config.lua stargate/config.lua") print("")
   os.execute("wget -f " .. Pfad .. "stargate/check.lua stargate/check.lua") print("")
-  os.execute("wget -f " .. Pfad .. "stargate/sprache.lua stargate/sprache/deutsch.lua") print("")
-  os.execute("wget -f " .. Pfad .. "stargate/sprache.lua stargate/sprache/english.lua") print("")
+  os.execute("wget -f " .. Pfad .. "stargate/sprache/deutsch.lua stargate/sprache/deutsch.lua") print("")
+  os.execute("wget -f " .. Pfad .. "stargate/sprache/english.lua stargate/sprache/english.lua") print("")
   os.execute("wget " .. Pfad .. "stargate/addressen.lua stargate/addressen.lua") print("")
   os.execute("wget " .. Pfad .. "sicherNachNeustart.lua stargate/sicherNachNeustart.lua") print("")
   f = io.open ("stargate/addressen.lua", "r")
