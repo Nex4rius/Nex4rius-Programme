c6aa0f80374513928913af3e232a59d95c39f48b
DarknessShadow <DarknessShadow@web.de>
2016-05-19 15:43:02 +0200

    Update suche_adressen.lua

--- stargate/suche_adressen.lua
+++ stargate/suche_adressen.lua
@@ -1,5 +1,6 @@
 component = require("component")
 sg = component.getPrimary("stargate")
+gpu = component.getPrimary("gpu")
 
 alleZeichen = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
 
@@ -24,7 +25,9 @@ for Ai = 1, 36 do
                   if sg.energyToDial(ersteStelle .. "ZV-L0Y-SA") == nil then
                     print(ersteStelle .. zweiteStelle .. dritteStelle .. vierteStelle .. "-" .. fuenfteStelle .. sechsteStelle .. siebteStelle .. "-" .. achteStelle .. neunteStelle)
                   else
+                    gpu.setForeground(0xFF0000)
                     print(ersteStelle .. zweiteStelle .. dritteStelle .. vierteStelle .. "-" .. fuenfteStelle .. sechsteStelle .. siebteStelle .. "-" .. achteStelle .. neunteStelle .. " GEFUNDEN")
+                    gpu.setForeground(0xFFFFFF)
                   end
                 end
               end
