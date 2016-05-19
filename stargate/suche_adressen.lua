b6b4d75be19c2c3bedf9b8c8bcaada37e2d6f99e
DarknessShadow <DarknessShadow@web.de>
2016-05-19 21:14:32 +0200

    Update suche_adressen.lua

--- stargate/suche_adressen.lua
+++ stargate/suche_adressen.lua
@@ -2,7 +2,9 @@ component = require("component")
 sg = component.getPrimary("stargate")
 gpu = component.getPrimary("gpu")
 
-alleZeichen = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
+alleZeichen = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+               "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
+               "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
 
 f = io.open("ergebnis_suche", "w")
 for Ai = 1, 36 do
