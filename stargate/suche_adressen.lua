956b4873cf70cf75a8e8f20dc25a0dd5bccc2c45
DarknessShadow <DarknessShadow@web.de>
2016-05-19 15:46:44 +0200

    Update suche_adressen.lua

--- stargate/suche_adressen.lua
+++ stargate/suche_adressen.lua
@@ -5,23 +5,23 @@ gpu = component.getPrimary("gpu")
 alleZeichen = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
 
 for Ai = 1, 36 do
-  ersteStelle = alleZeichen[Ai]
+  neunteStelle = alleZeichen[Ai]
   for Bi = 1, 36 do
-    zweiteStelle = alleZeichen[Bi]
+    achteStelle = alleZeichen[Bi]
     for Ci = 1, 36 do
-      dritteStelle = alleZeichen[Ci]
+      siebteStelle = alleZeichen[Ci]
       for Di = 1, 36 do
-        vierteStelle = alleZeichen[Di]
+        sechsteStelle = alleZeichen[Di]
         for Ei = 1, 36 do
           fuenfteStelle = alleZeichen[Ei]
           for Fi = 1, 36 do
-            sechsteStelle = alleZeichen[Fi]
+            vierteStelle = alleZeichen[Fi]
             for Gi = 1, 36 do
-              siebteStelle = alleZeichen[Gi]
+              dritteStelle = alleZeichen[Gi]
               for Hi = 1, 36 do
-                achteStelle = alleZeichen[Hi]
+                zweiteStelle = alleZeichen[Hi]
                 for Ii = 1, 36 do
-                  neunteStelle = alleZeichen[Ii]
+                  ersteStelle = alleZeichen[Ii]
                   if sg.energyToDial(ersteStelle .. "ZV-L0Y-SA") == nil then
                     print(ersteStelle .. zweiteStelle .. dritteStelle .. vierteStelle .. "-" .. fuenfteStelle .. sechsteStelle .. siebteStelle .. "-" .. achteStelle .. neunteStelle)
                   else
