bc9422c2b9a56e5531fda6a9228a92f405a9bd6d
DarknessShadow <DarknessShadow@web.de>
2016-05-22 09:03:19 +0200

    Update deutsch.lua

--- stargate/sprache/deutsch.lua
+++ stargate/sprache/deutsch.lua
@@ -82,4 +82,4 @@ StatusNameVerbunden       = "Verbunden"
 StatusNameSchliessend     = "Schließend"
 Neustart                  = "Neustart"
 verfuegbareSprachen       = "deutsch / english"
-dofile("/stargate/sprache/ersetzen.lua")
+dofile("stargate/sprache/ersetzen.lua")
