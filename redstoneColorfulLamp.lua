be561576343f711d77c06164f64e5202a0d419e0
DarknessShadow <DarknessShadow@web.de>
2016-06-04 08:47:18 +0200

    Update redstoneColorfulLamp.lua

--- redstoneColorfulLamp.lua
+++ redstoneColorfulLamp.lua
@@ -1,86 +1,86 @@
 component = require("component")
 event = require("event")
 r = component.getPrimary("redstone")
-Richtung = 0
+AusgangRichtung = 1
+EingangRichtung = 0
 
 a = true
 
 function rot()
   for i = 14, 10, -1 do
-    r.setBundledOutput(Richtung, i, 255)
+    r.setBundledOutput(AusgangRichtung, i, 255)
   end
   for i = 9, 0, -1 do
-    r.setBundledOutput(Richtung, i, 0)
+    r.setBundledOutput(AusgangRichtung, i, 0)
   end
 end
 
 function gelb()
   for i = 14, 4, -1 do
-    r.setBundledOutput(Richtung, i, 255)
+    r.setBundledOutput(AusgangRichtung, i, 255)
   end
   for i = 4, 0, -1 do
-    r.setBundledOutput(Richtung, i, 0)
+    r.setBundledOutput(AusgangRichtung, i, 0)
   end
 end
 
 function orange()
   for i = 9, 14 do
-    r.setBundledOutput(Richtung, i, 255)
+    r.setBundledOutput(AusgangRichtung, i, 255)
   end
   for i = 8, 0, -1 do
-    r.setBundledOutput(Richtung, i, 0)
+    r.setBundledOutput(AusgangRichtung, i, 0)
   end
 end
 
 function gruen()
   for i = 9, 5, -1 do
-    r.setBundledOutput(Richtung, i, 255)
+    r.setBundledOutput(AusgangRichtung, i, 255)
   end
   for i = 4, 0, -1 do
-    r.setBundledOutput(Richtung, i, 0)
+    r.setBundledOutput(AusgangRichtung, i, 0)
   end
   for i = 14, 10, -1 do
-    r.setBundledOutput(Richtung, i, 0)
+    r.setBundledOutput(AusgangRichtung, i, 0)
   end
 end
 
 function weiss()
   for i = 14, 0, -1 do
-    r.setBundledOutput(Richtung, i, 255)
+    r.setBundledOutput(AusgangRichtung, i, 255)
   end
 end
 
 function schwarz()
   for i = 14, 0, -1 do
-    r.setBundledOutput(Richtung, i, 0)
+    r.setBundledOutput(AusgangRichtung, i, 0)
   end
 end
 
+--getBundledInput(EingangRichtung, 0)  --weiß: Status nicht Inaktiv
+--getBundledInput(EingangRichtung, 14) --rot: eingehende Verbindung
+--getBundledInput(EingangRichtung, 4)  --gelb: Iris geschlossen
+--getBundledInput(EingangRichtung, 15) --schwarz: IDC akzeptiert
+--getBundledInput(EingangRichtung, 13) --grün: verbunden
+
 function redstone()
+  if getBundledInput(EingangRichtung, 4) > 0 then
+    rot()
+  elseif getBundledInput(EingangRichtung, 15) > 0 then
+    gruen()
+  elseif getBundledInput(EingangRichtung, 14) > 0 then
+    orange()
+  elseif getBundledInput(EingangRichtung, 13) > 0 then
+    gruen()
+  elseif getBundledInput(EingangRichtung, 0) > 0 then
+    gelb()
+  else
+    weiss()
+  end
 end
 
-weiss()
-
 --while a do
---for a = 0, 20 do
-  --redstone()
-  --event.pull(300)
---end
-
-print("weiss")
-weiss()
-os.sleep(2)
-print("rot")
-rot()
-os.sleep(2)
-print("gelb")
-gelb()
-os.sleep(2)
-print("orange")
-orange()
-os.sleep(2)
-print("gruen")
-gruen()
-os.sleep(2)
-print("schwarz")
-schwarz()
+for a = 0, 20 do
+  redstone()
+  event.pull(300)
+end
