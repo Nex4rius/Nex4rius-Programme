42093859eb421741bb2693dc7bc06a50954a7afc
DarknessShadow <DarknessShadow@web.de>
2016-06-12 11:50:05 +0200

    Update installieren.lua

--- installieren.lua
+++ installieren.lua
@@ -17,18 +17,19 @@ function installieren()
   fs.makeDirectory("/stargate/sprache")
   serverAddresse = "https://raw.githubusercontent.com/DarknessShadow/Stargate-Programm/"
   if versionTyp == nil then
-    versionTyp = "master/"
+    versionTyp = "master"
   end
-  os.execute("wget -f " .. Pfad() .. "autorun.lua autorun.lua") print("")
-  os.execute("wget -f " .. Pfad() .. "stargate/Kontrollprogramm.lua /stargate/Kontrollprogramm.lua") print("")
-  os.execute("wget -f " .. Pfad() .. "stargate/compat.lua /stargate/compat.lua") print("")
-  os.execute("wget -f " .. Pfad() .. "stargate/config.lua /stargate/config.lua") print("")
-  os.execute("wget -f " .. Pfad() .. "stargate/check.lua /stargate/check.lua") print("")
-  os.execute("wget -f " .. Pfad() .. "stargate/sprache/deutsch.lua /stargate/sprache/deutsch.lua") print("")
-  os.execute("wget -f " .. Pfad() .. "stargate/sprache/english.lua /stargate/sprache/english.lua") print("")
-  os.execute("wget -f " .. Pfad() .. "stargate/sprache/ersetzen.lua /stargate/sprache/ersetzen.lua") print("")
-  os.execute("wget    " .. Pfad() .. "stargate/adressen.lua /stargate/adressen.lua") print("")
-  os.execute("wget    " .. Pfad() .. "stargate/sicherNachNeustart.lua /stargate/sicherNachNeustart.lua") print("")
+  os.execute("wget -f " .. Pfad() .. "/autorun.lua autorun.lua") print("")
+  os.execute("wget -f " .. Pfad() .. "/stargate/Kontrollprogramm.lua /stargate/Kontrollprogramm.lua") print("")
+  os.execute("wget -f " .. Pfad() .. "/stargate/compat.lua /stargate/compat.lua") print("")
+  os.execute("wget -f " .. Pfad() .. "/stargate/config.lua /stargate/config.lua") print("")
+  os.execute("wget -f " .. Pfad() .. "/stargate/check.lua /stargate/check.lua") print("")
+  os.execute("wget -f " .. Pfad() .. "/stargate/version.txt /stargate/version.txt") print("")
+  os.execute("wget -f " .. Pfad() .. "/stargate/sprache/deutsch.lua /stargate/sprache/deutsch.lua") print("")
+  os.execute("wget -f " .. Pfad() .. "/stargate/sprache/english.lua /stargate/sprache/english.lua") print("")
+  os.execute("wget -f " .. Pfad() .. "/stargate/sprache/ersetzen.lua /stargate/sprache/ersetzen.lua") print("")
+  os.execute("wget    " .. Pfad() .. "/stargate/adressen.lua /stargate/adressen.lua") print("")
+  os.execute("wget    " .. Pfad() .. "/stargate/sicherNachNeustart.lua /stargate/sicherNachNeustart.lua") print("")
   f = io.open ("/stargate/adressen.lua", "r")
   addressRead = true
   leseLaenge = 1000
@@ -41,12 +42,20 @@ function installieren()
       addressRead = false
     end
   end
-  f:close ()
+  f:close()
   if string.sub(readAddresses, AdressesLength, AdressesLength) == " " then
     f = io.open ("/stargate/adressen.lua", "a")
     f:seek ("end", -1)
     f:write("")
-    f:close ()
+    f:close()
+  end
+  if versionTyp == "beta" then
+    f = io.open ("/stargate/version.txt", "r")
+    version = f:read()
+    f:close()
+    f = io.open ("/stargate/version.txt", "w")
+    f:write(version .. " BETA")
+    f:close()
   end
   os.execute("del -v installieren.lua")
   installieren = true
@@ -60,7 +69,7 @@ function schreibSicherungsdatei()
   f:write('firstrun = ' .. firstrun .. '\n')
   f:write('Sprache = "' .. Sprache .. '" -- deutsch / english\n')
   f:write('installieren = ' .. tostring(installieren) .. '\n')
-  f:close ()
+  f:close()
 end
 
 installieren()
