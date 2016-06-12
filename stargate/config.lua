7daa72fdd6e0d8bd3a73453869f8c20b0247283a
DarknessShadow <DarknessShadow@web.de>
2016-06-12 11:28:46 +0200

    Update config.lua

--- stargate/config.lua
+++ stargate/config.lua
@@ -1,7 +1,7 @@
 ZeitBeimEinschalten   = os.time()
 os.sleep(1)
 sectime               = ZeitBeimEinschalten - os.time()
-letzteNachricht       = c.uptime()
+letzteNachricht       = os.time()
 enteridc              = ""
 showidc               = ""
 remoteName            = ""
