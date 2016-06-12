f591050a913df969e7b091bbdfee797ef7f94c96
DarknessShadow <DarknessShadow@web.de>
2016-06-12 09:41:20 +0200

    Update installieren.lua

--- installieren.lua
+++ installieren.lua
@@ -17,18 +17,18 @@ function installieren()
   fs.makeDirectory("/stargate/sprache")
   serverAddresse = "https://raw.githubusercontent.com/DarknessShadow/Stargate-Programm/"
   if versionTyp == nil then
-    versionTyp = "master/"
+    versionTyp = "master"
   end
-  os.execute("wget -f " .. Pfad() .. "autorun.lua autorun.lua") print("")
-  os.execute("wget -f " .. Pfad() .. "stargate/Kontrollprogramm.lua /stargate/Kontrollprogramm.lua") print("")
-  os.execute("wget -f " .. Pfad() .. "stargate/compat.lua /stargate/compat.lua") print("")
-  os.execute("wget -f " .. Pfad() .. "stargate/config.lua /stargate/config.lua") print("")
-  os.execute("wget -f " .. Pfad() .. "stargate/check.lua /stargate/check.lua") print("")
-  os.execute("wget -f " .. Pfad() .. "stargate/sprache/deutsch.lua /stargate/sprache/deutsch.lua") print("")
-  os.execute("wget -f " .. Pfad() .. "stargate/sprache/english.lua /stargate/sprache/english.lua") print("")
-  os.execute("wget -f " .. Pfad() .. "stargate/sprache/ersetzen.lua /stargate/sprache/ersetzen.lua") print("")
-  os.execute("wget    " .. Pfad() .. "stargate/adressen.lua /stargate/adressen.lua") print("")
-  os.execute("wget    " .. Pfad() .. "stargate/sicherNachNeustart.lua /stargate/sicherNachNeustart.lua") print("")
+  os.execute("wget -f " .. Pfad() .. "/autorun.lua autorun.lua") print("")
+  os.execute("wget -f " .. Pfad() .. "/stargate/Kontrollprogramm.lua /stargate/Kontrollprogramm.lua") print("")
+  os.execute("wget -f " .. Pfad() .. "/stargate/compat.lua /stargate/compat.lua") print("")
+  os.execute("wget -f " .. Pfad() .. "/stargate/config.lua /stargate/config.lua") print("")
+  os.execute("wget -f " .. Pfad() .. "/stargate/check.lua /stargate/check.lua") print("")
+  os.execute("wget -f " .. Pfad() .. "/stargate/sprache/deutsch.lua /stargate/sprache/deutsch.lua") print("")
+  os.execute("wget -f " .. Pfad() .. "/stargate/sprache/english.lua /stargate/sprache/english.lua") print("")
+  os.execute("wget -f " .. Pfad() .. "/stargate/sprache/ersetzen.lua /stargate/sprache/ersetzen.lua") print("")
+  os.execute("wget    " .. Pfad() .. "/stargate/adressen.lua /stargate/adressen.lua") print("")
+  os.execute("wget    " .. Pfad() .. "/stargate/sicherNachNeustart.lua /stargate/sicherNachNeustart.lua") print("")
   f = io.open ("/stargate/adressen.lua", "r")
   addressRead = true
   leseLaenge = 1000
