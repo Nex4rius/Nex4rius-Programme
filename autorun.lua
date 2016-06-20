1439f9984805d52a72653da392619fff0caae1eb
DarknessShadow <DarknessShadow@web.de>
2016-06-20 14:22:11 +0200

    Update autorun.lua

--- autorun.lua
+++ autorun.lua
@@ -1,4 +1,3 @@
 -- pastebin run -f ySJv3YyT
 local shell = require("shell")
-local args = shell.parse(...)
-os.execute("stargate/check.lua" .. args)
+os.execute("stargate/check.lua" .. shell.parse(...))
