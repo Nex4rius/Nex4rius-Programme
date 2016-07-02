fbe1882e557162132ee5471ac06f499d15d51224
Nexarius <DarknessShadow@web.de>
2016-07-02 17:22:07 +0200

    Update adressen.lua

--- stargate/adressen.lua
+++ stargate/adressen.lua
@@ -5,12 +5,15 @@
 --  to save press "Ctrl + S"
 --  to close press "Ctrl + W"
 --
---  Put your own stargate addresses here
---  "" for no Iris Code
+-- Put your own stargate addresses here
+-- "" for no Iris Code
+--
+-- Adress Format
+-- {"<Name>", "<Adresse>", "<IDC>"},
 --
 
 return {
---{"<Name>", "<Adress>", "<IDC>"},
-  {"Name 1", "Adress 1", "IDC 1"},
-  {"Name 2", "Adress 2", "IDC 2"},
+--{"<Name>", "<Adresse>", "<IDC>"},
+  {"Name 1", "Adresse 1", "IDC 1"},
+  {"Name 2", "Adresse 2", "IDC 2"},
 }
