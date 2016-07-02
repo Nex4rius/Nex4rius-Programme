6ae41ef43fdabe8233004d61b175b1e7910e55a9
Nexarius <DarknessShadow@web.de>
2016-07-02 18:12:37 +0200

    Update installieren.lua

--- installieren.lua
+++ installieren.lua
@@ -4,6 +4,7 @@
 
 fs = require("filesystem")
 wget = loadfile("/bin/wget.lua")
+schreibSicherungsdatei = loadfile("/stargate/schreibSicherungsdatei.lua")
 serverAddresse = "https://raw.githubusercontent.com/Nex4rius/Stargate-Programm/"
 
 if fs.exists("/stargate/Sicherungsdatei.lua") then
@@ -56,6 +57,7 @@ function installieren()
   wget("-f", Pfad(versionTyp) .. "/stargate/check.lua", "/stargate/check.lua")
   wget("-f", Pfad(versionTyp) .. "/stargate/version.txt", "/stargate/version.txt")
   wget("-f", Pfad(versionTyp) .. "/stargate/Kontrollprogramm.lua", "/stargate/Kontrollprogramm.lua")
+  wget("-f", Pfad(versionTyp) .. "/stargate/schreibSicherungsdatei.lua", "/stargate/schreibSicherungsdatei.lua")
   wget("-f", Pfad(versionTyp) .. "/stargate/sprache/deutsch.lua", "/stargate/sprache/deutsch.lua")
   wget("-f", Pfad(versionTyp) .. "/stargate/sprache/english.lua", "/stargate/sprache/english.lua")
   wget("-f", Pfad(versionTyp) .. "/stargate/sprache/ersetzen.lua", "/stargate/sprache/ersetzen.lua")
@@ -75,38 +77,8 @@ function installieren()
   end
   os.execute("del -v installieren.lua")
   installieren = true
-  schreibSicherungsdatei()
+  schreibSicherungsdatei(IDC, autoclosetime, RF, Sprache, side, installieren, control)
   os.execute("reboot")
-end
-
-function schreibSicherungsdatei()
-  f = io.open ("/stargate/Sicherungsdatei.lua", "w")
-  f:write('-- pastebin run -f fa9gu1GJ\n')
-  f:write('-- von Nex4rius\n')
-  f:write('-- https://github.com/Nex4rius/Stargate-Programm\n--\n')
-  f:write('-- to save press "Ctrl + S"\n')
-  f:write('-- to close press "Ctrl + W"\n--\n\n')
-  f:write('local IDC = "' .. tostring(IDC) .. '" -- Iris Deactivation Code\n')
-  f:write('local autoclosetime = ' .. tostring(autoclosetime) .. ' -- in seconds -- false for no autoclose\n')
-  f:write('local RF = ' .. tostring(RF) .. ' -- show energy in RF instead of EU\n')
-  f:write('local Sprache = "' .. tostring(Sprache) .. '" -- deutsch / english\n')
-  f:write('local side = "' .. tostring(side) .. '" -- bottom, top, back, front, right or left\n\n')
-  f:write(string.rep("-", 70) .. '\n\n')
-  f:write('local installieren = ' .. tostring(installieren) .. '\n')
-  f:write('local control = "' .. tostring(control) .. '"\n')
-  f:write('local firstrun = ' .. tostring(firstrun) .. '\n\n')
-  f:write(string.rep("-", 70) .. '\n\n')
-  f:write('if type(IDC) ~= "string" then\n  IDC = ""\nend\n')
-  f:write('if type(autoclosetime) ~= "number" then\n  autoclosetime = false\nend\n')
-  f:write('if type(RF) ~= "boolean" then\n  RF = false\nend\n')
-  f:write('if type(Sprache) ~= "string" then\n  Sprache = ""\nend\n')
-  f:write('if type(side) ~= "string" then\n  side = "unten"\nend\n')
-  f:write('if type(installieren) ~= "boolean" then\n  installieren = false\nend\n')
-  f:write('if type(control) ~= "string" then\n  control = "On"\nend\n')
-  f:write('if type(firstrun) ~= "number" then\n  firstrun = -2\nend\n')
-  f:write('if type(IDC) ~= "string" then\n  IDC = ""\nend\n\n')
-  f:write('return IDC, autoclosetime, RF, Sprache, side, installieren, control, firstrun\n')
-  f:close()
 end
 
 installieren()
