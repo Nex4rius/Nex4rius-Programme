bc437d3fe5ee6aab95080ff504cbebb8ca57079f
Nexarius <DarknessShadow@web.de>
2016-07-02 10:43:41 +0200

    Update adressen.lua

--- stargate/adressen.lua
+++ stargate/adressen.lua
@@ -8,18 +8,8 @@
 --  Put your own stargate addresses here
 --  "" for no Iris Code
 --
---  Iris will open if correct code is received
---  change it to whatever you want
---  for no IDC set it to "" (Iris will never open automatically)
---
 
-RF = false -- show energy in RF instead of EU
-autoclosetime = 60 -- in seconds -- false for no autoclose
-side = "bottom" -- bottom, top, back, front, right or left
-
-IDC = "" -- Iris Deactivation Code
-
-adressen = {
+return {
 --{"<Name>", "<Adress>", "<IDC>"},
   {"Name 1", "Adress 1", "IDC 1"},
   {"Name 2", "Adress 2", "IDC 2"},
