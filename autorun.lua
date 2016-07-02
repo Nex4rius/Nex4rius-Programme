5b9ff4bb98ea8d837d080f0f151de15083a6b6bd
Nexarius <DarknessShadow@web.de>
2016-07-02 16:03:11 +0200

    Update autorun.lua

--- autorun.lua
+++ autorun.lua
@@ -7,11 +7,7 @@ local args = shell.parse(...)
 if type(args[1]) == "string" then
   args[1] = string.lower(args[1])
 else
-  args[1] = nil
+  args[1] = ""
 end
 
-if not args[1] then
-  os.execute("stargate/check.lua")
-else
-  os.execute("stargate/check.lua " .. args[1])
-end
+os.execute("stargate/check.lua " .. args[1])
