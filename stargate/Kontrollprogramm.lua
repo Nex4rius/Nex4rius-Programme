d3fd0b1fa063fc9a1505b3031a300b34e57f2cd0
Nexarius <DarknessShadow@web.de>
2016-07-02 11:19:16 +0200

    Update Kontrollprogramm.lua

--- stargate/Kontrollprogramm.lua
+++ stargate/Kontrollprogramm.lua
@@ -152,16 +152,32 @@ if sg.irisState() == "Offline" then
 end
 
 function schreibSicherungsdatei()
-  _ENV.control      = control
-  _ENV.firstrun     = firstrun
-  _ENV.Sprache      = Sprache
-  _ENV.installieren = installieren
-  f = io.open ("/stargate/sicherNachNeustart.lua", "w")
-  f:write("-- pastebin run -f fa9gu1GJ\n-- von Nex4rius\n-- https://github.com/Nex4rius/Stargate-Programm\n\n")
-  f:write('control = "' .. _ENV.control .. '"\n')
-  f:write('firstrun = ' .. _ENV.firstrun .. '\n')
-  f:write('Sprache = "' .. _ENV.Sprache .. '" -- deutsch / english\n')
-  f:write('installieren = ' .. tostring(_ENV.installieren) .. '\n')
+  f = io.open ("/stargate/Sicherungsdatei.lua", "w")
+  f:write('-- pastebin run -f fa9gu1GJ\n')
+  f:write('-- von Nex4rius\n')
+  f:write('-- https://github.com/Nex4rius/Stargate-Programm\n--\n')
+  f:write('-- to save press "Ctrl + S"\n')
+  f:write('-- to close press "Ctrl + W"\n--\n\n')
+  f:write('local IDC = "' .. tostring(IDC) .. '" -- Iris Deactivation Code\n')
+  f:write('local autoclosetime = ' .. tostring(autoclosetime) .. ' -- in seconds -- false for no autoclose\n')
+  f:write('local RF = ' .. tostring(false) .. ' -- show energy in RF instead of EU\n')
+  f:write('local Sprache = "' .. tostring(Sprache) .. '" -- deutsch / english\n')
+  f:write('local side = "' .. tostring(side) .. '" -- bottom, top, back, front, right or left\n\n')
+  f:write(string.rep("-", 70) .. '\n\n')
+  f:write('local installieren = ' .. tostring(false) .. '\n')
+  f:write('local control = "' .. tostring(control) .. '"\n')
+  f:write('local firstrun = ' .. tostring(firstrun) .. '\n\n')
+  f:write(string.rep("-", 70) .. '\n\n')
+  f:write('if type(IDC) ~= "string" then\n  IDC = ""\nend\n')
+  f:write('if type(autoclosetime) ~= "number" then\n  autoclosetime = false\nend\n')
+  f:write('if type(RF) ~= "boolean" then\n  RF = false\nend\n')
+  f:write('if type(Sprache) ~= "string" then\n  Sprache = ""\nend\n')
+  f:write('if type(side) ~= "string" then\n  side = "unten"\nend\n')
+  f:write('if type(installieren) ~= "boolean" then\n  installieren = false\nend\n')
+  f:write('if type(control) ~= "string" then\n  control = "On"\nend\n')
+  f:write('if type(firstrun) ~= "number" then\n  firstrun = -2\nend\n')
+  f:write('if type(IDC) ~= "string" then\n  IDC = ""\nend\n\n')
+  f:write('return IDC, autoclosetime, RF, Sprache, side, installieren, control, firstrun\n')
   f:close()
 end
 
