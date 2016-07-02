dec844ae8e500f0fc5773f6af1625e4a8a7f865f
Nexarius <DarknessShadow@web.de>
2016-07-02 14:37:38 +0200

    Update Kontrollprogramm.lua

--- stargate/Kontrollprogramm.lua
+++ stargate/Kontrollprogramm.lua
@@ -145,36 +145,6 @@ if sg.irisState() == "Offline" then
   Trennlinienhoehe    = 13
 end
 
-function schreibSicherungsdatei()
-  f = io.open ("/stargate/Sicherungsdatei.lua", "w")
-  f:write('-- pastebin run -f fa9gu1GJ\n')
-  f:write('-- von Nex4rius\n')
-  f:write('-- https://github.com/Nex4rius/Stargate-Programm\n--\n')
-  f:write('-- to save press "Ctrl + S"\n')
-  f:write('-- to close press "Ctrl + W"\n--\n\n')
-  f:write('local IDC = "' .. tostring(IDC) .. '" -- Iris Deactivation Code\n')
-  f:write('local autoclosetime = ' .. tostring(autoclosetime) .. ' -- in seconds -- false for no autoclose\n')
-  f:write('local RF = ' .. tostring(false) .. ' -- show energy in RF instead of EU\n')
-  f:write('local Sprache = "' .. tostring(Sprache) .. '" -- deutsch / english\n')
-  f:write('local side = "' .. tostring(side) .. '" -- bottom, top, back, front, right or left\n\n')
-  f:write(string.rep("-", 70) .. '\n\n')
-  f:write('local installieren = ' .. tostring(false) .. '\n')
-  f:write('local control = "' .. tostring(control) .. '"\n')
-  f:write('local firstrun = ' .. tostring(firstrun) .. '\n\n')
-  f:write(string.rep("-", 70) .. '\n\n')
-  f:write('if type(IDC) ~= "string" then\n  IDC = ""\nend\n')
-  f:write('if type(autoclosetime) ~= "number" then\n  autoclosetime = false\nend\n')
-  f:write('if type(RF) ~= "boolean" then\n  RF = false\nend\n')
-  f:write('if type(Sprache) ~= "string" then\n  Sprache = ""\nend\n')
-  f:write('if type(side) ~= "string" then\n  side = "unten"\nend\n')
-  f:write('if type(installieren) ~= "boolean" then\n  installieren = false\nend\n')
-  f:write('if type(control) ~= "string" then\n  control = "On"\nend\n')
-  f:write('if type(firstrun) ~= "number" then\n  firstrun = -2\nend\n')
-  f:write('if type(IDC) ~= "string" then\n  IDC = ""\nend\n\n')
-  f:write('return IDC, autoclosetime, RF, Sprache, side, installieren, control, firstrun\n')
-  f:close()
-end
-
 local function try(func, ...)
   local ok, result = pcall(func, ...)
   if not ok then
@@ -605,7 +575,7 @@ function newAddress(g)
     f:close ()
     AddNewAddress = false
     firstrun = -1
-    schreibSicherungsdatei()
+    schreibSicherungsdatei(IDC, autoclosetime, RF, Sprache, side, installieren, control, firstrun)
     AdressenSpeichern()
     IDC, autoclosetime, RF, Sprache, side = loadfile("/stargate/Sicherungsdatei.lua")()
     sides()
@@ -1001,11 +971,11 @@ handlers[key_event_name] = function(e)
         if control == "On" then
           control = "Off"
           _ENV.control = "Off"
-          schreibSicherungsdatei()
+          schreibSicherungsdatei(IDC, autoclosetime, RF, Sprache, side, installieren, control, firstrun)
         else
           control = "On"
           _ENV.control = "On"
-          schreibSicherungsdatei()
+          schreibSicherungsdatei(IDC, autoclosetime, RF, Sprache, side, installieren, control, firstrun)
         end
       end
     elseif c == "z" then
@@ -1026,12 +996,8 @@ handlers[key_event_name] = function(e)
       antwortFrageSprache = io.read()
       if string.lower(antwortFrageSprache) == "deutsch" or string.lower(antwortFrageSprache) == "english" then
         Sprache = string.lower(antwortFrageSprache)
-        _ENV.control = control
-        _ENV.firstrun = firstrun
-        _ENV.Sprache = Sprache
-        _ENV.installieren = true
         installieren = true
-        schreibSicherungsdatei()
+        schreibSicherungsdatei(IDC, autoclosetime, RF, Sprache, side, installieren, control, firstrun)
         zeigeNachricht(Sprachaenderung)
       else
         print(fehlerName)
