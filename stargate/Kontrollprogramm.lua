d2b80cf78114fc48a0fe9c4417869919c23f6de9
Nexarius <DarknessShadow@web.de>
2016-07-03 11:03:11 +0200

    Update Kontrollprogramm.lua

--- stargate/Kontrollprogramm.lua
+++ stargate/Kontrollprogramm.lua
@@ -590,9 +590,8 @@ end
 
 function newAddress(neueAdresse)
   if AddNewAddress == true then
-    --adressen[AdressenAnzahl + 1] = '">>' .. neueAdresse .. '<<", "' .. neueAdresse .. '", ""'
     adressen[AdressenAnzahl + 1] = {}
-    adressen[AdressenAnzahl + 1][1] = ">>" .. neueAdresse .. "<<"
+    adressen[AdressenAnzahl + 1][1] = ">>>" .. neueAdresse .. "<<<"
     adressen[AdressenAnzahl + 1][2] = neueAdresse
     adressen[AdressenAnzahl + 1][3] = ""
     schreibeAdressen()
