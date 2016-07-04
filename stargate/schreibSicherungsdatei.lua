9dddfb6c9f62f3e9f248a74f90aec3698ae451f0
Nexarius <DarknessShadow@web.de>
2016-07-04 10:33:46 +0200

    Update schreibSicherungsdatei.lua

--- stargate/schreibSicherungsdatei.lua
+++ stargate/schreibSicherungsdatei.lua
@@ -6,8 +6,9 @@ local Sprache       = args[4]
 local side          = args[5]
 local installieren  = args[6]
 local control       = args[7]
+local autoUpdate    = args[8]
 
-for i = 1, 7 do
+for i = 1, 8 do
   if args[i] == nil then
     return false
   end
@@ -19,14 +20,15 @@ f:write('-- von Nex4rius\n')
 f:write('-- https://github.com/Nex4rius/Stargate-Programm\n--\n')
 f:write('-- to save press "Ctrl + S"\n')
 f:write('-- to close press "Ctrl + W"\n--\n\n')
-f:write('local IDC = "' .. tostring(IDC) .. '" -- Iris Deactivation Code\n')
+f:write('local IDC           = "' .. tostring(IDC) .. '" -- Iris Deactivation Code\n')
 f:write('local autoclosetime = ' .. tostring(autoclosetime) .. ' -- in seconds -- false for no autoclose\n')
-f:write('local RF = ' .. tostring(RF) .. ' -- show energy in RF instead of EU\n')
-f:write('local Sprache = "' .. tostring(Sprache) .. '" -- deutsch / english\n')
-f:write('local side = "' .. tostring(side) .. '" -- bottom, top, back, front, right or left\n\n')
-f:write(string.rep("-", 70) .. '\n\n')
-f:write('local installieren = ' .. tostring(installieren) .. '\n')
-f:write('local control = "' .. tostring(control) .. '"\n\n')
+f:write('local RF            = ' .. tostring(RF) .. ' -- show energy in RF instead of EU\n')
+f:write('local Sprache       = "' .. tostring(Sprache) .. '" -- deutsch / english\n')
+f:write('local side          = "' .. tostring(side) .. '" -- bottom, top, back, front, right or left\n\n')
+f:write('local autoUpdate    = true -- automatically updates the programm\n')
+f:write(string.rep("-", 10) .. "don't change anything below" .. string.rep("-", 33) .. '\n\n')
+f:write('local installieren  = ' .. tostring(installieren) .. '\n')
+f:write('local control       = "' .. tostring(control) .. '"\n\n')
 f:write(string.rep("-", 70) .. '\n\n')
 f:write('if type(IDC) ~= "string" then\n  IDC = ""\nend\n')
 f:write('if type(autoclosetime) ~= "number" then\n  autoclosetime = 60\nend\n')
@@ -36,7 +38,8 @@ f:write('if type(side) ~= "string" then\n  side = "unten"\nend\n')
 f:write('if type(installieren) ~= "boolean" then\n  installieren = false\nend\n')
 f:write('if type(control) ~= "string" then\n  control = "On"\nend\n')
 f:write('if type(IDC) ~= "string" then\n  IDC = ""\nend\n\n')
-f:write('return IDC, autoclosetime, RF, Sprache, side, installieren, control\n')
+f:write('if type(autoUpdate) ~= "boolean" then\n  autoUpdate = false\nend\n')
+f:write('return IDC, autoclosetime, RF, Sprache, side, installieren, control, autoUpdate\n')
 f:close()
 
 return true
