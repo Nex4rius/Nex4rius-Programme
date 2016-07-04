aed56bcdd860fe98a940a2452dc0c8516e91c54b
Nexarius <DarknessShadow@web.de>
2016-07-04 14:45:08 +0200

    Update installieren.lua

--- installieren.lua
+++ installieren.lua
@@ -49,9 +49,9 @@ local function installieren()
   loadfile("/bin/rm.lua")("-v", "installieren.lua")
   installieren = true
   loadfile("/stargate/schreibSicherungsdatei.lua")(IDC, autoclosetime, RF, Sprache, side, installieren, control, autoUpdate)
-  loadfile("/autorun.lua")("no")
-  os.exit()
-  --require("computer").shutdown(true)
+  --loadfile("/autorun.lua")("no")
+  --os.exit()
+  require("computer").shutdown(true)
 end
 
 if fs.exists("/stargate/sicherNachNeustart.lua") then
