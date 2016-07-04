e9623d1350ddcff1628eeb55cbe9010d0b5bd89a
Nexarius <DarknessShadow@web.de>
2016-07-04 10:52:48 +0200

    Update schreibSicherungsdatei.lua

--- stargate/schreibSicherungsdatei.lua
+++ stargate/schreibSicherungsdatei.lua
@@ -1,18 +1,25 @@
-local args          = require("shell").parse(...)
-local IDC           = args[1]
-local autoclosetime = args[2]
-local RF            = args[3]
-local Sprache       = args[4]
-local side          = args[5]
-local installieren  = args[6]
-local control       = args[7]
-local autoUpdate    = args[8]
+local args  = require("shell").parse(...)
+local fs    = require("filesystem")
 
-for i = 1, 8 do
-  if args[i] == nil then
-    return false
+if fs.exists("/stargate/Sicherungsdatei.lua") then
+  local IDC, autoclosetime, RF, Sprache, side, installieren, control, autoUpdate = loadfile("/stargate/Sicherungsdatei.lua")()
+else
+  for i = 1, 8 do
+    if args[i] == nil then
+      return false
+    end
   end
+  local IDC, autoclosetime, RF, Sprache, side, installieren, control, autoUpdate
 end
+
+if type(args[1]) == "string"  then IDC            = args[1] end
+if type(args[2]) == "number"  then autoclosetime  = args[2] end
+if type(args[3]) == "boolean" then RF             = args[3] end
+if type(args[4]) == "string"  then Sprache        = args[4] end
+if type(args[5]) == "string"  then side           = args[5] end
+if type(args[6]) == "boolean" then installieren   = args[6] end
+if type(args[7]) == "string"  then control        = args[7] end
+if type(args[8]) == "boolean" then autoUpdate     = args[8] end
 
 f = io.open ("/stargate/Sicherungsdatei.lua", "w")
 f:write('-- pastebin run -f fa9gu1GJ\n')
@@ -21,13 +28,13 @@ f:write('-- https://github.com/Nex4rius/Stargate-Programm\n--\n')
 f:write('-- to save press "Ctrl + S"\n')
 f:write('-- to close press "Ctrl + W"\n--\n\n')
 f:write('local IDC           = "' .. tostring(IDC) .. '" -- Iris Deactivation Code\n')
-f:write('local autoclosetime = ' .. tostring(autoclosetime) .. ' -- in seconds -- false for no autoclose\n')
-f:write('local RF            = ' .. tostring(RF) .. ' -- show energy in RF instead of EU\n')
+f:write('local autoclosetime = '  .. tostring(autoclosetime) .. ' -- in seconds -- false for no autoclose\n')
+f:write('local RF            = '  .. tostring(RF) .. ' -- show energy in RF instead of EU\n')
 f:write('local Sprache       = "' .. tostring(Sprache) .. '" -- deutsch / english\n')
 f:write('local side          = "' .. tostring(side) .. '" -- bottom, top, back, front, right or left\n\n')
 f:write('local autoUpdate    = true -- automatically updates the programm\n')
 f:write(string.rep("-", 10) .. "don't change anything below" .. string.rep("-", 33) .. '\n\n')
-f:write('local installieren  = ' .. tostring(installieren) .. '\n')
+f:write('local installieren  = '  .. tostring(installieren) .. '\n')
 f:write('local control       = "' .. tostring(control) .. '"\n\n')
 f:write(string.rep("-", 70) .. '\n\n')
 f:write('if type(IDC) ~= "string" then\n  IDC = ""\nend\n')
