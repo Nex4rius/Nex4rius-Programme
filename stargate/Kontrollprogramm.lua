e31680342f267040bdf03f5f7b5dc0680279f157
Nexarius <DarknessShadow@web.de>
2016-07-05 21:31:08 +0200

    Update Kontrollprogramm.lua

--- stargate/Kontrollprogramm.lua
+++ stargate/Kontrollprogramm.lua
@@ -153,8 +153,7 @@ local function schreibeAdressen()
   f:write('return {\n')
   f:write('--{"<Name>", "<Adresse>", "<IDC>"},\n')
   for k, v in pairs(adressen) do
-    --f:write("  " .. require("serialization").serialize(adressen[k]) .. ",\n")
-    f:write(string.format('  {"%s", "%s", "%s"},', adressen[k][1], adressen[k][2], adressen[k][3]))
+    f:write(string.format('  {"%s", "%s", "%s"},\n', adressen[k][1], adressen[k][2], adressen[k][3]))
   end
   f:write('}')
   f:close()
