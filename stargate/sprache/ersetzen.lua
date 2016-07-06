821aad1269da04fe3c242c21933c114a9b8a11ff
Nexarius <DarknessShadow@web.de>
2016-07-06 12:04:51 +0200

    Update ersetzen.lua

--- stargate/sprache/ersetzen.lua
+++ stargate/sprache/ersetzen.lua
@@ -2,15 +2,25 @@
 -- von Nex4rius
 -- https://github.com/Nex4rius/Stargate-Programm
 
+local args = require("shell").parse(...)
+
+if type(args[1]) == "string" then
+  Sprache = string.lower(args[1])
+else
+  return false
+end
+
+local sprachen = loadfile("/stargate/sprache/" .. Sprache .. ".lua")()
+
 return {
-  ["On"]                  = irisKontrolleNameAn,
-  ["Off"]                 = irisKontrolleNameAus,
-  ["Open"]                = irisNameOffen,
-  ["Opening"]             = irisNameOeffnend,
-  ["Closed"]              = irisNameGeschlossen,
-  ["Closing"]             = irisNameSchliessend,
-  ["Offline"]             = irisNameOffline,
-  ["Override"]            = Eingriff,
-  ["Manual"]              = manueller,
-  ["Control"]             = IrisSteuerungName,
+  ["On"]                  = sprachen.irisKontrolleNameAn,
+  ["Off"]                 = sprachen.irisKontrolleNameAus,
+  ["Open"]                = sprachen.irisNameOffen,
+  ["Opening"]             = sprachen.irisNameOeffnend,
+  ["Closed"]              = sprachen.irisNameGeschlossen,
+  ["Closing"]             = sprachen.irisNameSchliessend,
+  ["Offline"]             = sprachen.irisNameOffline,
+  ["Override"]            = sprachen.Eingriff,
+  ["Manual"]              = sprachen.manueller,
+  ["Control"]             = sprachen.IrisSteuerungName,
 }
