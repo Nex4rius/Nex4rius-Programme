d3c571f909bbed3878c01a07259fd2b7f88651ee
Nexarius <DarknessShadow@web.de>
2016-07-10 04:57:29 +0200

    Update check.lua

--- Stargate-Programm/stargate/check.lua
+++ Stargate-Programm/stargate/check.lua
@@ -97,6 +97,7 @@ function update(versionTyp)
     installieren = true
     schreibSicherungsdatei(IDC, autoclosetime, RF, Sprache, side, installieren, control, autoUpdate)
     f = io.open ("autorun.lua", "w")
+    f:write('versionTyp = "' .. versionTyp .. '"\n')
     f:write('loadfile("installieren.lua")(' .. versionTyp .. ')')
     f:close()
     loadfile("autorun.lua")()
