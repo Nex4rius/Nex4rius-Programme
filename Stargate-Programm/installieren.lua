d1ee8b3bb04dd5c1f20ce7d16c8bb45b4f2fc9af
Nexarius <DarknessShadow@web.de>
2016-07-10 05:25:01 +0200

    Update installieren.lua

--- Stargate-Programm/installieren.lua
+++ Stargate-Programm/installieren.lua
@@ -1,4 +1,4 @@
--- pastebin run -f 1pbsaeCQ
+-- pastebin run -f wLK1gCKt
 -- von Nex4rius
 -- https://github.com/Nex4rius/Stargate-Programm
 
@@ -27,7 +27,7 @@ else
 end
 
 f = io.open ("/autorun.lua", "w")
-f:write('-- pastebin run -f 1pbsaeCQ\n')
+f:write('-- pastebin run -f wLK1gCKt\n')
 f:write('-- von Nex4rius\n')
 f:write('-- https://github.com/Nex4rius/Stargate-Programm\n\n')
 f:write('local args = require("shell").parse(...)[1]\n\n')
@@ -99,7 +99,7 @@ if fs.exists("/stargate/sicherNachNeustart.lua") then
     dofile("/stargate/adressen.lua")
   end
   f = io.open("/stargate/adressen.lua", "w")
-  f:write('-- pastebin run -f 1pbsaeCQ\n')
+  f:write('-- pastebin run -f wLK1gCKt\n')
   f:write('-- von Nex4rius\n')
   f:write('-- https://github.com/Nex4rius/Stargate-Programm\n--\n')
   f:write('-- to save press "Ctrl + S"\n')
