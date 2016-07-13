c8ee5a7ae65e5c53a1e936d6491aac041688a281
Nexarius <DarknessShadow@web.de>
2016-07-13 20:43:43 +0200

    Update schreibSicherungsdatei.lua

--- Stargate-Programm/stargate/schreibSicherungsdatei.lua
+++ Stargate-Programm/stargate/schreibSicherungsdatei.lua
@@ -13,14 +13,14 @@ if type(SicherungNEU) ~= "table" then
   SicherungNEU = {}
 end
 
-if SicherungNEU.IDC          == nil and SicherungALT.IDC          ~= nil then SicherungNEU.IDC          = SicherungALT.IDC           else SicherungNEU.IDC           = ""      end
-if SicherungNEU.RF           == nil and SicherungALT.RF           ~= nil then SicherungNEU.RF           = SicherungALT.RF            else SicherungNEU.RF            = false   end
-if SicherungNEU.side         == nil and SicherungALT.side         ~= nil then SicherungNEU.side         = SicherungALT.side          else SicherungNEU.side          = "unten" end
-if SicherungNEU.Sprache      == nil and SicherungALT.Sprache      ~= nil then SicherungNEU.Sprache      = SicherungALT.Sprache       else SicherungNEU.Sprache       = ""      end
-if SicherungNEU.autoUpdate   == nil and SicherungALT.autoUpdate   ~= nil then SicherungNEU.autoUpdate   = SicherungALT.autoUpdate    else SicherungNEU.autoUpdate    = true    end
-if SicherungNEU.autoclosetime== nil and SicherungALT.autoclosetime~= nil then SicherungNEU.autoclosetime= SicherungALT.autoclosetime else SicherungNEU.autoclosetime = 60      end
-if SicherungNEU.control      == nil and SicherungALT.control      ~= nil then SicherungNEU.control      = SicherungALT.control       else SicherungNEU.control       = "On"    end
-if SicherungNEU.installieren == nil and SicherungALT.installieren ~= nil then SicherungNEU.installieren = SicherungALT.installieren  else SicherungNEU.installieren  = false   end
+if SicherungNEU.IDC          == nil and SicherungALT.IDC          ~= nil then SicherungNEU.IDC          = SicherungALT.IDC           else SicherungNEU.IDC           = ""       end
+if SicherungNEU.RF           == nil and SicherungALT.RF           ~= nil then SicherungNEU.RF           = SicherungALT.RF            else SicherungNEU.RF            = false    end
+if SicherungNEU.side         == nil and SicherungALT.side         ~= nil then SicherungNEU.side         = SicherungALT.side          else SicherungNEU.side          = "unten"  end
+if SicherungNEU.Sprache      == nil and SicherungALT.Sprache      ~= nil then SicherungNEU.Sprache      = SicherungALT.Sprache       else SicherungNEU.Sprache       = "deutsch"end
+if SicherungNEU.autoUpdate   == nil and SicherungALT.autoUpdate   ~= nil then SicherungNEU.autoUpdate   = SicherungALT.autoUpdate    else SicherungNEU.autoUpdate    = true     end
+if SicherungNEU.autoclosetime== nil and SicherungALT.autoclosetime~= nil then SicherungNEU.autoclosetime= SicherungALT.autoclosetime else SicherungNEU.autoclosetime = 60       end
+if SicherungNEU.control      == nil and SicherungALT.control      ~= nil then SicherungNEU.control      = SicherungALT.control       else SicherungNEU.control       = "On"     end
+if SicherungNEU.installieren == nil and SicherungALT.installieren ~= nil then SicherungNEU.installieren = SicherungALT.installieren  else SicherungNEU.installieren  = false    end
   
 f = io.open ("/stargate/Sicherungsdatei.lua", "w")
 f:write('-- pastebin run -f wLK1gCKt\n')
