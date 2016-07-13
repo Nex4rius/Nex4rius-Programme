21de717cc158f64e17b08149a40f7bdeef123e2b
Nexarius <DarknessShadow@web.de>
2016-07-13 11:36:06 +0200

    Update schreibSicherungsdatei.lua

--- Stargate-Programm/stargate/schreibSicherungsdatei.lua
+++ Stargate-Programm/stargate/schreibSicherungsdatei.lua
@@ -2,22 +2,26 @@
 -- von Nex4rius
 -- https://github.com/Nex4rius/Stargate-Programm/tree/master/Stargate-Programm
 
-local args = require("shell").parse(...)
-local IDC, autoclosetime, RF, Sprache, side, installieren, control, autoUpdate
+local SicherungNEU = require("shell").parse(...)[1]
+local SicherungALT = {}
 
 if require("filesystem").exists("/stargate/Sicherungsdatei.lua") then
-  IDC, autoclosetime, RF, Sprache, side, installieren, control, autoUpdate = loadfile("/stargate/Sicherungsdatei.lua")()
+  SicherungALT = loadfile("/stargate/Sicherungsdatei.lua")()
 end
 
-if args[1] ~= nil then IDC           = args[1] elseif IDC           == nil then IDC           = ""      end
-if args[2] ~= nil then autoclosetime = args[2] elseif autoclosetime == nil then autoclosetime = 60      end
-if args[3] ~= nil then RF            = args[3] elseif RF            == nil then RF            = false   end
-if args[4] ~= nil then Sprache       = args[4] elseif Sprache       == nil then Sprache       = ""      end
-if args[5] ~= nil then side          = args[5] elseif side          == nil then side          = "unten" end
-if args[6] ~= nil then installieren  = args[6] elseif installieren  == nil then installieren  = false   end
-if args[7] ~= nil then control       = args[7] elseif control       == nil then control       = "On"    end
-if args[8] ~= nil then autoUpdate    = args[8] elseif autoUpdate    == nil then autoUpdate    = true   end
+if type(SicherungNEU) ~= "table" then
+  SicherungNEU = {}
+end
 
+if SicherungNEU.IDC          == nil and SicherungALT.IDC          ~= nil then SicherungNEU.IDC          = SicherungALT.IDC           else SicherungNEU.IDC           = ""      end
+if SicherungNEU.RF           == nil and SicherungALT.RF           ~= nil then SicherungNEU.RF           = SicherungALT.RF            else SicherungNEU.RF            = false   end
+if SicherungNEU.side         == nil and SicherungALT.side         ~= nil then SicherungNEU.side         = SicherungALT.side          else SicherungNEU.side          = "unten" end
+if SicherungNEU.Sprache      == nil and SicherungALT.Sprache      ~= nil then SicherungNEU.Sprache      = SicherungALT.Sprache       else SicherungNEU.Sprache       = ""      end
+if SicherungNEU.autoUpdate   == nil and SicherungALT.autoUpdate   ~= nil then SicherungNEU.autoUpdate   = SicherungALT.autoUpdate    else SicherungNEU.autoUpdate    = true    end
+if SicherungNEU.autoclosetime== nil and SicherungALT.autoclosetime~= nil then SicherungNEU.autoclosetime= SicherungALT.autoclosetime else SicherungNEU.autoclosetime = 60      end
+if SicherungNEU.installieren == nil and SicherungALT.installieren ~= nil then SicherungNEU.installieren = SicherungALT.installieren  else SicherungNEU.installieren  = false   end
+if SicherungNEU.control      == nil and SicherungALT.control      ~= nil then SicherungNEU.control      = SicherungALT.control       else SicherungNEU.control       = "On"    end
+  
 f = io.open ("/stargate/Sicherungsdatei.lua", "w")
 f:write('-- pastebin run -f wLK1gCKt\n')
 f:write('-- von Nex4rius\n')
@@ -25,16 +29,14 @@ f:write('-- https://github.com/Nex4rius/Stargate-Programm/tree/master/Stargate-P
 f:write('-- to save press "Ctrl + S"\n')
 f:write('-- to close press "Ctrl + W"\n--\n\n')
 f:write('return {\n')
-f:write('  IDC           = "' .. tostring(IDC) .. '" -- Iris Deactivation Code\n')
-f:write('  autoclosetime = '  .. tostring(autoclosetime) .. ' -- in seconds -- false for no autoclose\n')
-f:write('  RF            = '  .. tostring(RF) .. ' -- show energy in RF instead of EU\n')
-f:write('  Sprache       = "' .. tostring(Sprache) .. '" -- deutsch / english\n')
-f:write('  side          = "' .. tostring(side) .. '" -- bottom, top, back, front, right or left\n')
-f:write('  autoUpdate    = '  .. tostring(autoUpdate) .. ' -- automatically updates the programm\n\n')
+f:write('  IDC           = "' .. tostring(SicherungNEU.IDC) .. '" -- Iris Deactivation Code\n')
+f:write('  autoclosetime = '  .. tostring(SicherungNEU.autoclosetime) .. ' -- in seconds -- false for no autoclose\n')
+f:write('  RF            = '  .. tostring(SicherungNEU.RF) .. ' -- show energy in RF instead of EU\n')
+f:write('  Sprache       = "' .. tostring(SicherungNEU.Sprache) .. '" -- deutsch / english\n')
+f:write('  side          = "' .. tostring(SicherungNEU.side) .. '" -- bottom, top, back, front, right or left\n')
+f:write('  autoUpdate    = '  .. tostring(SicherungNEU.autoUpdate) .. ' -- automatically updates the programm\n\n')
 f:write(string.rep("-", 10) .. "don't change anything below" .. string.rep("-", 33) .. '\n\n')
-f:write('  installieren  = '  .. tostring(installieren) .. '\n')
-f:write('  control       = "' .. tostring(control) .. '"\n\n')
+f:write('  installieren  = '  .. tostring(SicherungNEU.installieren) .. '\n')
+f:write('  control       = "' .. tostring(SicherungNEU.control) .. '"\n\n')
 f:write('}')
 f:close()
-
-return true
