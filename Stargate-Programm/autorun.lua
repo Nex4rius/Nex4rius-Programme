b2a1f42a9b83b78269bacea843f87017f7810638
Nexarius <DarknessShadow@web.de>
2016-07-14 10:32:03 +0200

    Update autorun.lua

--- Stargate-Programm/autorun.lua
+++ Stargate-Programm/autorun.lua
@@ -1,6 +1,6 @@
 -- pastebin run -f Dkt9dn4S
 -- von Nex4rius
--- https://github.com/Nex4rius/Nex4rius-Programme/blob/beta/Stargate-Programm
+-- https://github.com/Nex4rius/Nex4rius-Programme/tree/master/Stargate-Programm
 
 local args = require("shell").parse(...)[1]
 
