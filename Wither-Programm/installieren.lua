416959dd17c27b3852927a831693882cd60a4130
Nexarius <DarknessShadow@web.de>
2016-07-24 16:48:12 +0200

    Update installieren.lua

--- Wither-Programm/installieren.lua
+++ Wither-Programm/installieren.lua
@@ -1,56 +1,33 @@
 -- pastebin run -f j9duLPvp
 -- von Nex4rius
--- https://github.com/Nex4rius/Nex4rius-Programme/tree/master/Stargate-Programm
+-- https://github.com/Nex4rius/Nex4rius-Programme/tree/master/Wither-Programm
 
 local fs          = require("filesystem")
 local arg         = require("shell").parse(...)[1]
 local wget        = loadfile("/bin/wget.lua")
 local move        = loadfile("/bin/mv.lua")
-local Sicherung   = {}
 local Funktionen  = {}
-local sprachen
-local IDC, autoclosetime, RF, Sprache, side, installieren, control, autoUpdate
-
-if fs.exists("/stargate/Sicherungsdatei.lua") then
-  Sicherung = loadfile("/stargate/Sicherungsdatei.lua")()
-  if type(Sicherung) == "string" then
-    Sicherung = {}
-    Sicherung.IDC, Sicherung.autoclosetime, Sicherung.RF, Sicherung.Sprache, Sicherung.side, Sicherung.installieren, Sicherung.control, Sicherung.autoUpdate = loadfile("/stargate/Sicherungsdatei.lua")()
-  end
-else
-  Sicherung.Sprache = ""
-  Sicherung.installieren = false
-end
-
-if Sicherung.Sprache then
-  if fs.exists("/stargate/sprache/" .. Sicherung.Sprache .. ".lua") then
-    sprachen = loadfile("/stargate/sprache/" .. Sicherung.Sprache .. ".lua")()
-  end
-end
 
 function Funktionen.Pfad(versionTyp)
   if versionTyp then
-    return "https://raw.githubusercontent.com/Nex4rius/Stargate-Programm/" .. versionTyp .. "/Stargate-Programm/"
+    return "https://raw.githubusercontent.com/Nex4rius/Wither-Programm/" .. versionTyp .. "/Wither-Programm/"
   else
-    return "https://raw.githubusercontent.com/Nex4rius/Stargate-Programm/master/Stargate-Programm/"
+    return "https://raw.githubusercontent.com/Nex4rius/Wither-Programm/master/Wither-Programm/"
   end
 end
 
 function Funktionen.installieren(versionTyp)
-  fs.makeDirectory("/update/stargate/sprache")
+  fs.makeDirectory("/update/wither/sprache")
   local updateKomplett = false
   local update = {}
-  update[1] = wget("-f", Funktionen.Pfad(versionTyp) .. "autorun.lua",                        "/update/autorun.lua")
-  update[2] = wget("-f", Funktionen.Pfad(versionTyp) .. "stargate/check.lua",                 "/update/stargate/check.lua")
-  update[3] = wget("-f", Funktionen.Pfad(versionTyp) .. "stargate/version.txt",               "/update/stargate/version.txt")
-  update[4] = wget("-f", Funktionen.Pfad(versionTyp) .. "stargate/adressen.lua",              "/update/stargate/adressen.lua")
-  update[5] = wget("-f", Funktionen.Pfad(versionTyp) .. "stargate/Sicherungsdatei.lua",       "/update/stargate/Sicherungsdatei.lua")
-  update[6] = wget("-f", Funktionen.Pfad(versionTyp) .. "stargate/Kontrollprogramm.lua",      "/update/stargate/Kontrollprogramm.lua")
-  update[7] = wget("-f", Funktionen.Pfad(versionTyp) .. "stargate/schreibSicherungsdatei.lua","/update/stargate/schreibSicherungsdatei.lua")
-  update[8] = wget("-f", Funktionen.Pfad(versionTyp) .. "stargate/sprache/deutsch.lua",       "/update/stargate/sprache/deutsch.lua")
-  update[9] = wget("-f", Funktionen.Pfad(versionTyp) .. "stargate/sprache/english.lua",       "/update/stargate/sprache/english.lua")
-  update[10]= wget("-f", Funktionen.Pfad(versionTyp) .. "stargate/sprache/ersetzen.lua",      "/update/stargate/sprache/ersetzen.lua")
-  for i = 1, 10 do
+  update[1] = wget("-f", Funktionen.Pfad(versionTyp) .. "autorun.lua",                   "/update/autorun.lua")
+  update[2] = wget("-f", Funktionen.Pfad(versionTyp) .. "wither/check.lua",              "/update/wither/check.lua")
+  update[3] = wget("-f", Funktionen.Pfad(versionTyp) .. "wither/version.txt",            "/update/wither/version.txt")
+  update[4] = wget("-f", Funktionen.Pfad(versionTyp) .. "wither/wither.lua",             "/update/wither/wither.lua")
+  update[5] = wget("-f", Funktionen.Pfad(versionTyp) .. "wither/sprache/deutsch.lua",    "/update/wither/sprache/deutsch.lua")
+  update[6] = wget("-f", Funktionen.Pfad(versionTyp) .. "wither/sprache/english.lua",    "/update/wither/sprache/english.lua")
+  update[7] = wget("-f", Funktionen.Pfad(versionTyp) .. "wither/sicherNachNeustart.lua", "/update/wither/sicherNachNeustart.lua")
+  for i in pairs(update) do
     if update[i] then
       updateKomplett = true
     else
@@ -59,46 +36,38 @@ function Funktionen.installieren(versionTyp)
         print(sprachen.fehlerName .. i)
       end
       local f = io.open ("/autorun.lua", "w")
-      f:write('-- pastebin run -f Dkt9dn4S\n')
+      f:write('-- pastebin run -f j9duLPvp\n')
       f:write('-- von Nex4rius\n')
-      f:write('-- https://github.com/Nex4rius/Nex4rius-Programme/tree/master/Stargate-Programm\n\n')
+      f:write('-- https://github.com/Nex4rius/Nex4rius-Programme/tree/master/Wither-Programm\n\n')
       f:write('local args = require("shell").parse(...)[1]\n\n')
       f:write('if type(args) == "string" then\n')
-      f:write('  loadfile("/stargate/check.lua")(args)\n')
+      f:write('  loadfile("/wither/check.lua")(args)\n')
       f:write('else\n')
-      f:write('  loadfile("/stargate/check.lua")()\n')
+      f:write('  loadfile("/wither/check.lua")()\n')
       f:write('end\n\n')
       f:close()
       break
     end
   end
   if updateKomplett then
-    fs.makeDirectory("/stargate/sprache")
-    move("-f", "/update/autorun.lua",                         "/autorun.lua")
-    move("-f", "/update/stargate/check.lua",                  "/stargate/check.lua")
-    move("-f", "/update/stargate/version.txt",                "/stargate/version.txt")
-    if fs.exists("/stargate/adressen.lua") == false then
-      move(    "/update/stargate/adressen.lua",               "/stargate/adressen.lua")
+    fs.makeDirectory("/wither/sprache")
+    move("-f", "/update/autorun.lua",                "/autorun.lua")
+    move("-f", "/update/wither/check.lua",           "/wither/check.lua")
+    move("-f", "/update/wither/version.txt",         "/wither/version.txt")
+    if fs.exists("/wither/Sicherungsdatei.lua") == false then
+      move(    "/update/wither/Sicherungsdatei.lua", "/wither/Sicherungsdatei.lua")
     end
-    if fs.exists("/stargate/Sicherungsdatei.lua") == false then
-      move(    "/update/stargate/Sicherungsdatei.lua",        "/stargate/Sicherungsdatei.lua")
-    end
-    move("-f", "/update/stargate/Kontrollprogramm.lua",       "/stargate/Kontrollprogramm.lua")
-    move("-f", "/update/stargate/schreibSicherungsdatei.lua", "/stargate/schreibSicherungsdatei.lua")
-    move("-f", "/update/stargate/sprache/deutsch.lua",        "/stargate/sprache/deutsch.lua")
-    move("-f", "/update/stargate/sprache/english.lua",        "/stargate/sprache/english.lua")
-    move("-f", "/update/stargate/sprache/ersetzen.lua",       "/stargate/sprache/ersetzen.lua")
-    f = io.open ("/stargate/version.txt", "r")
+    move("-f", "/update/wither/sprache/deutsch.lua", "/wither/sprache/deutsch.lua")
+    move("-f", "/update/wither/sprache/english.lua", "/wither/sprache/english.lua")
+    f = io.open ("/wither/version.txt", "r")
     version = f:read()
     f:close()
     if versionTyp == "beta" then
-      f = io.open ("/stargate/version.txt", "w")
+      f = io.open ("/wither/version.txt", "w")
       f:write(version .. " BETA")
       f:close()
     end
   end
-  Sicherung.installieren = true
-  loadfile("/stargate/schreibSicherungsdatei.lua")(Sicherung)
   print()
   loadfile("/bin/rm.lua")("-v", "/update")
   loadfile("/bin/rm.lua")("-v", "/installieren.lua")
