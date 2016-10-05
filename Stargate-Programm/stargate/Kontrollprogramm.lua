07125a46820afa40fcbedb0e4490aae411e65393
Nexarius <DarknessShadow@web.de>
2016-10-05 16:54:40 +0200

    Update Kontrollprogramm.lua

--- Stargate-Programm/stargate/Kontrollprogramm.lua
+++ Stargate-Programm/stargate/Kontrollprogramm.lua
@@ -285,7 +285,7 @@ function Funktion.AdressenLesen()
       if AdressAnzeige == 10 then
         AdressAnzeige = 0
       end
-      if na[2] == remAddr then
+      if na[2] == remAddr and string.len(tostring(remAddr)) > 5 then
         gpu.setBackground(Farben.AdressfarbeAktiv)
       end
       Funktion.zeigeHier(1, y, AdressAnzeige .. " " .. string.sub(na[1], 1, xVerschiebung - 7), 28 - string.len(string.sub(na[1], 1, xVerschiebung - 7)))
