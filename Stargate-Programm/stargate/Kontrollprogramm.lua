8f6a08c226f1b070f9d433b3f8ed7fcd1b390e46
Nexarius <DarknessShadow@web.de>
2016-10-05 16:27:51 +0200

    Update Kontrollprogramm.lua

--- Stargate-Programm/stargate/Kontrollprogramm.lua
+++ Stargate-Programm/stargate/Kontrollprogramm.lua
@@ -58,7 +58,7 @@ local LampenRot                 = false
 local VersionUpdate             = false
 
 Farben.graueFarbe               = 6684774
-Farben.hellblau                 = 0x808080
+Farben.hellblau                 = 0x606060
 Farben.hellgrau                 = 8421504
 Farben.roteFarbe                = 0xFF0000
 Farben.weisseFarbe              = 0xFFFFFF
