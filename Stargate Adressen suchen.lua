10d270c3fd749b04dbf36d69f610f0ba8a80ca64
Nexarius <DarknessShadow@web.de>
2016-10-09 13:07:59 +0200

    Update Stargate Adressen suchen.lua

--- Stargate Adressen suchen.lua
+++ Stargate Adressen suchen.lua
@@ -4,6 +4,13 @@ local alleZeichen = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
                      "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
                      "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
 
+-- test
+local function suchen(...)
+  for i = 1, 36 do
+    return alleZeichen[i]
+  end
+end
+
 for Ai = 1, 36 do
   local A = alleZeichen[Ai]
   for Bi = 1, 36 do
