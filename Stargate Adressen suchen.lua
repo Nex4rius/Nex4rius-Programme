10b467b309604512c2c24be4c97b08ebce194db7
Nexarius <DarknessShadow@web.de>
2016-10-23 22:55:07 +0200

    Update Stargate Adressen suchen.lua

--- Stargate Adressen suchen.lua
+++ Stargate Adressen suchen.lua
@@ -4,13 +4,6 @@ local alleZeichen = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
                      "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
                      "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
 
--- test
-local function suchen(...)
-  for i = 1, 36 do
-    return alleZeichen[i]
-  end
-end
-
 for Ai = 1, 36 do
   local A = alleZeichen[Ai]
   for Bi = 1, 36 do
@@ -30,15 +23,15 @@ for Ai = 1, 36 do
                 for Ii = 1, 36 do
                   local I = alleZeichen[Ii]
                   local Adresse = I .. H .. G .. F .. "-" .. E .. D .. C .. "-" .. B .. A
-                  if sg.energyToDial(Adresse) == nil then
-                    print(Adresse)
-                  else
+                  if sg.energyToDial(Adresse) then
                     require("component").getPrimary("gpu").setForeground(0xFF0000)
                     print("\n" .. Adresse .. " GEFUNDEN\n")
                     f = io.open("ergebnis_suche", "a")
                     f:write(Adresse .. "\n")
                     f:close ()
                     require("component").getPrimary("gpu").setForeground(0xFFFFFF)
+                  else
+                    print(Adresse)
                   end
                 end
               end
