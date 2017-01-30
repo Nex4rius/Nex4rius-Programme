50032061e7e0e9e6486d0000e6ac2845901507ef
Nexarius <DarknessShadow@web.de>
2017-01-30 18:22:09 +0100

    Update farben.lua

--- Tank Anzeige/farben.lua
+++ Tank Anzeige/farben.lua
@@ -2,7 +2,7 @@ return {
   Water     = {schriftAN = 0x000000, hinterAN = 0x1E90FF, schriftAUS = 0xFFFFFF, hinterAUS = 0x22348E},
   Lava      = {schriftAN = 0x000000, hinterAN = 0xDF8E2A, schriftAUS = 0x000000, hinterAUS = 0xCC460A},
   Chlorine  = {schriftAN = 0xFFFFFF, hinterAN = 0x317D7D, schriftAUS = 0xFFFFFF, hinterAUS = 0x295A5A},
-  Oil       = {schriftAN = 0xFFFFFF, hinterAN = 0x18181E, schriftAUS = 0xFFFFFF, hinterAUS = 0x010101},
+  Oil       = {schriftAN = 0xFFFFFF, hinterAN = 0x010101, schriftAUS = 0xFFFFFF, hinterAUS = 0x18181E},
   Liquid_XP = {schriftAN = 0x000000, hinterAN = 0x8DFC0E, schriftAUS = 0x000000, hinterAUS = 0x50FC01},
   Radon     = {schriftAN = 0xFFFFFF, hinterAN = 0xEE54FA, schriftAUS = 0xFFFFFF, hinterAUS = 0xDE44EA},
   unbekannt = {schriftAN = 0xFFFFFF, hinterAN = 0x444444, schriftAUS = 0xFFFFFF, hinterAUS = 0x333333},
