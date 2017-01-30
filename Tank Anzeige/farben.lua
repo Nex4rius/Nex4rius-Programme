e288929a11d260346f378461df78eedf8c186ebc
Nexarius <DarknessShadow@web.de>
2017-01-30 16:58:13 +0100

    Create farben.lua

---
+++ Tank Anzeige/farben.lua
@@ -0,0 +1,5 @@
+return {
+Water     = {schriftAN = 0xFFFFFF, hinterAN = 0x1E90FF, schriftAUS = 0xFFFFFF, hinterAUS = 0x00008B},
+Lava      = {schriftAN = 0x000000, hinterAN = 0xFF4500, schriftAUS = 0x000000, hinterAUS = 0xFFA500},
+unbekannt = {schriftAN = 0xFFFFFF, hinterAN = 0x444444, schriftAUS = 0xFFFFFF, hinterAUS = 0x333333},
+}
