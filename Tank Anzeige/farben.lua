980fc7f51d16c257eb7b755afbd1f612b4435565
Nexarius <DarknessShadow@web.de>
2017-01-30 17:12:25 +0100

    Update farben.lua

--- Tank Anzeige/farben.lua
+++ Tank Anzeige/farben.lua
@@ -1,5 +1,7 @@
 return {
-Water     = {schriftAN = 0xFFFFFF, hinterAN = 0x1E90FF, schriftAUS = 0xFFFFFF, hinterAUS = 0x00008B},
-Lava      = {schriftAN = 0x000000, hinterAN = 0xFF4500, schriftAUS = 0x000000, hinterAUS = 0xFFA500},
+Water     = {schriftAN = 0x000000, hinterAN = 0x1E90FF, schriftAUS = 0x000000, hinterAUS = 0x22348E},
+Lava      = {schriftAN = 0x000000, hinterAN = 0xDF8E2A, schriftAUS = 0x000000, hinterAUS = 0xCC460A},
+--Oil       = {schriftAN = 0xFFFFFF, hinterAN = 0x18181E, schriftAUS = 0xFFFFFF, hinterAUS = 0x010101},
+--XP        = {schriftAN = 0x000000, hinterAN = 0x8DFC0E, schriftAUS = 0x000000, hinterAUS = 0x50FC01},
 unbekannt = {schriftAN = 0xFFFFFF, hinterAN = 0x444444, schriftAUS = 0xFFFFFF, hinterAUS = 0x333333},
 }
