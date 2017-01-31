7fc539f4e5865c0376d0d2e397732614e47c3f39
Nexarius <DarknessShadow@web.de>
2017-01-31 12:33:02 +0100

    Create autorun.lua

---
+++ Tank/server/autorun.lua
@@ -0,0 +1,15 @@
+-- pastebin run -f cyF0yhXZ\n')
+-- von Nex4rius\n')
+-- https://github.com/Nex4rius/Nex4rius-Programme\n')
+
+local shell = require("shell")
+local alterPfad = shell.getWorkingDirectory("/")
+local args = shell.parse(...)[1]
+
+shell.setWorkingDirectory("/")
+
+if type(args) == "string" then
+  loadfile("/tank/auslesen.lua")(args)
+else\n')
+  loadfile("/tank/auslesen.lua")()
+end
