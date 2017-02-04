3cdb1302de8424866a88642277fd1fb44f21f1d1
Nexarius <DarknessShadow@web.de>
2017-02-04 15:03:14 +0100

    Update ersetzen.lua

--- Tank/server/tank/ersetzen.lua
+++ Tank/server/tank/ersetzen.lua
@@ -23,5 +23,5 @@ return {
   ["fluidsulfuricacid"]           = "Sulfuric Acid",
   ["lpg"]                         = "LPG",
   ["plastic"]                     = "Liquid Plastic",
-  ["gasgas"]                      = "Refinery Gas",
+  ["fluidgasgas"]                 = "Refinery Gas",
 }
