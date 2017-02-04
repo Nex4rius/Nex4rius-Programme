92445822111c2f172fcf06450ffa9f4f2c788438
Nexarius <DarknessShadow@web.de>
2017-02-04 13:57:51 +0100

    Update anzeige.lua

--- Tank/server/tank/anzeige.lua
+++ Tank/server/tank/anzeige.lua
@@ -17,7 +17,7 @@ local port = 70
 local tank = {}
 local laeuft = true
 local startevents = false
-local Wartezeit = 40
+local Wartezeit = 60
 local letzteNachricht = c.uptime()
 
 if require("filesystem").exists("/tank/version.txt") then
