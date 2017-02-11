9880ed63bdcffccf3549cbe99f41d55db3c9ac13
Nexarius <DarknessShadow@web.de>
2017-02-11 18:58:06 +0100

    Update standby.lua

--- standby.lua
+++ standby.lua
@@ -2,4 +2,44 @@
 -- von Nex4rius
 -- https://github.com/Nex4rius/Nex4rius-Programme
 
-local standby = false
+local arg = require("shell").parse(...)[1]
+local component = require("component")
+local computer = require("computer")
+local standby = 0.75
+local display = 0.50
+local wartezeit = 30
+local screen
+
+if component.isAvailable("screen") then
+  screen = component.getPrimary("screen")
+end
+
+if type(arg) == "table" then
+  if type(arg.standby) == "number" then
+    standby = arg.standby
+  end
+  if type(arg.display) == "number" then
+    display = arg.display
+  end
+  if type(arg.wartezeit) == "number" then
+    wartezeit = arg.wartezeit
+  end
+  arg = nil
+end
+
+local function energie()
+  return computer.energy() / computer.maxEnergy()
+end
+
+while energie() < standby do
+  if energie() < display and screen then
+    screen.turnOff()
+  end
+  os.sleep(wartezeit)
+end
+
+if screen then
+  screen.turnOn()
+end
+
+return true
