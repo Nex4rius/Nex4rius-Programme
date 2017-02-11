a7be457b46c928c81c9f0962bc05c5a11f360877
Nexarius <DarknessShadow@web.de>
2017-02-11 19:06:02 +0100

    Update standby.lua

--- standby.lua
+++ standby.lua
@@ -2,44 +2,44 @@
 -- von Nex4rius
 -- https://github.com/Nex4rius/Nex4rius-Programme
 
-local arg = require("shell").parse(...)[1]
-local component = require("component")
-local computer = require("computer")
-local standby = 0.75
-local display = 0.50
-local wartezeit = 30
-local screen
+local function standby(arg)
+  local component = require("component")
+  local computer = require("computer")
+  local standby = 0.75
+  local display = 0.50
+  local wartezeit = 30
+  local screen
 
-if component.isAvailable("screen") then
-  screen = component.getPrimary("screen")
-end
-
-if type(arg) == "table" then
-  if type(arg.standby) == "number" then
-    standby = arg.standby
+  if component.isAvailable("screen") then
+    screen = component.getPrimary("screen")
   end
-  if type(arg.display) == "number" then
-    display = arg.display
+
+  if type(arg) == "table" then
+    if type(arg.standby) == "number" then
+      standby = arg.standby
+    end
+    if type(arg.display) == "number" then
+      display = arg.display
+    end
+    if type(arg.wartezeit) == "number" then
+      wartezeit = arg.wartezeit
+    end
   end
-  if type(arg.wartezeit) == "number" then
-    wartezeit = arg.wartezeit
+
+  local function energie()
+    return computer.energy() / computer.maxEnergy()
   end
-  arg = nil
-end
 
-local function energie()
-  return computer.energy() / computer.maxEnergy()
-end
-
-while energie() < standby do
-  if energie() < display and screen then
-    screen.turnOff()
+  while energie() < standby do
+    if energie() < display and screen then
+      screen.turnOff()
+    end
+    os.sleep(wartezeit)
   end
-  os.sleep(wartezeit)
+
+  if screen then
+    screen.turnOn()
+  end
 end
 
-if screen then
-  screen.turnOn()
-end
-
-return true
+return standby
