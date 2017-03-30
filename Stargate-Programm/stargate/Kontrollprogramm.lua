9e03835727911e4b80604d05edc6f0e54674b322
Nexarius <DarknessShadow@web.de>
2017-03-30 19:13:42 +0200

    Update Kontrollprogramm.lua

--- Stargate-Programm/stargate/Kontrollprogramm.lua
+++ Stargate-Programm/stargate/Kontrollprogramm.lua
@@ -645,7 +645,7 @@ end
 
 function Funktion.destinationName()
   if state == "Dialling" or state == "Connected" and type(adressen) == "table" then
-    if remoteName == "" and state == "Dialling" and wormhole == "in" then
+    if remoteName == "" and wormhole == "in" then
       for j, na in pairs(adressen) do
         if remAddr == na[2] then
           if na[1] == na[2] then
