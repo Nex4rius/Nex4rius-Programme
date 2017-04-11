8b2da727b2d81fc8f358bfc929993e9448cc5e36
Nexarius <DarknessShadow@web.de>
2017-04-11 10:30:09 +0200

    Update updater.lua

--- Updater/updater.lua
+++ Updater/updater.lua
@@ -16,7 +16,7 @@ local wget          = loadfile("/bin/wget.lua")
 local kopieren      = loadfile("/bin/cp.lua")
 local entfernen     = loadfile("/bin/rm.lua")
 
-local alterPfad     = shell.getWorkingDirectory("/")
+local alterPfad     = shell.getWorkingDirectory()
 
 local Funktion      = {}
 local hilfe         = false
@@ -51,27 +51,39 @@ elseif type(args1) == "string" and type(args2) == "string" and type(args3) == "s
     if args4 then
         link = args4
     end
-elseif args1 == "hilfe" or args1 == "help" or args1 == "?" then
+elseif args1 == "hilfe" or args1 == "help" or args1 == "?" or not args1 then
     hilfe = true
 else
+    gpu.setForeground(0xFF0000)
     print("<FEHLER> falsche Eingabe")
     hilfe = true
 end
 
---https://api.github.com/repos/Nex4rius/Nex4rius-Programme/branches/master
---https://api.github.com/repos/Nex4rius/Nex4rius-Programme/git/trees/3e24c5cfe4824cdecfb0641cefff8ecacb4bc28e?recursive=1
---https://api.github.com/repos/Nex4rius/Nex4rius-Programme/git/trees/master?recursive=1
-
 function Funktion.Pfad(nummer)
     if nummer == "1" then
-        return string.format("https://api.github.com/repos/%s/%s/branches/%s", name, repo, tree)
+        return "https://raw.githubusercontent.com/Nex4rius/Nex4rius-Programme/master/"
     elseif nummer == "2" then
-        return string.format("https://api.github.com/repos/%s/%s/git/trees/%s?recursive=1", name, repo, sha)
+        return string.format("https://api.github.com/repos/%s/%s/git/trees/%s?recursive=1", name, repo, tree)
     elseif nummer == "3" then
         return string.format("https://raw.githubusercontent.com/%s/%s/%s/", name, repo, tree)
-    elseif nummer == "4" then
-        return "https://raw.githubusercontent.com/Nex4rius/Nex4rius-Programme/master/"
     end
+end
+
+function Funktion.Hilfe()
+    require("term").clear()
+    print("Benutzung: /updater [name] [repo] [tree] [link]")
+    print("name, repo, tree, link von GitHub")
+    print()
+    print("Beispiele:")
+    print("/updater Nex4rius Nex4rius-Programme master Stargate-Programm/")
+    print("/updater MightyPirates OpenComputers master-MC1.7.10 src/main/resources/assets/opencomputers/loot/openos/")
+    print()
+    print("integrierte Befehle:")
+    print("/updater stargate")
+    print("/updater openos")
+    print()
+    print("Hilfetext")
+    print("/updater hilfe")
 end
 
 function Funktion.checkKomponenten()
@@ -91,8 +103,8 @@ function Funktion.checkKomponenten()
         end
     end
     local alleKomponenten = {
-        {"internet", "- Internet             ok", "- Internet             fehlt", true},
-        {"gpu",      "- GPU                  ok", "- GPU                  fehlt"},
+        {"internet", "- Internet   ok", "- Internet   fehlt", true},
+        {"gpu",      "- GPU        ok", "- GPU        fehlt"},
     }
     for i in pairs(alleKomponenten) do
         check(alleKomponenten[i])
@@ -105,49 +117,56 @@ function Funktion.checkKomponenten()
 end
 
 function Funktion.verarbeiten()
-    if not wget("-f", Funktion.Pfad("1"), "/github-liste.txt") then return end
+    if not wget("-f", Funktion.Pfad("2"), "/github-liste.txt") then
+        gpu.setForeground(0xFF0000)
+        print("<FEHLER> GitHub Download")
+        return 
+    end
     local f = io.open("/github-liste.txt", "r")
-    sha = loadfile("/json.lua")():decode(f:read("*all")).commit.sha
-    f:close()
-    if not wget("-f", Funktion.Pfad("2"), "/github-liste.txt") then return end
-    local f = io.open("/github-liste.txt", "r")
+    print("Konvertiere: JSON -> Lua table\n")
     local dateien = loadfile("/json.lua")():decode(f:read("*all"))
     f:close()
     fs.makeDirectory("/update")
     local komplett = true
+    print("Erstelle Verzeichnisse\n")
     for i in pairs(dateien.tree) do
-        if dateien.tree[i].type == "tree" then
+        if dateien.tree[i].type == "tree" and link:find(dateien.tree[i].path) then
             fs.makeDirectory("/update/" .. dateien.tree[i].path)
+            print("/update/" .. dateien.tree[i].path)
         end
     end
+    print("\nStarte Download\n")
     for i in pairs(dateien.tree) do
-        if dateien.tree[i].type == "blob" then
+        if dateien.tree[i].type == "blob" and link:find(dateien.tree[i].path) then
             if not wget("-f", Funktion.Pfad("3") .. dateien.tree[i].path, "/update/" .. dateien.tree[i].path) then
                 komplett = false
                 break
             end
         end
     end
+    gpu.setForeground(0x00FF00)
     print("\nDownload Beendet\n")
     if dateien["truncated"] or not komplett then
         gpu.setForeground(0xFF0000)
         print("<FEHLER> Download unvollständig")
+        gpu.setForeground(0xFFFFFF)
         entfernen("-rv", "/update")
-        --entfernen("-rv", "/github-liste.txt")
+        entfernen("-rv", "/github-liste.txt")
         shell.setWorkingDirectory(alterPfad)
         os.exit()
     else
+        gpu.setForeground(0xFFFFFF)
         print("Ersetze alte Dateien\n")
         for i in fs.list("/update") do
             kopieren("-rv", "/update/" .. i, "/")
         end
-        entfernen("-rv", "/update")
-        entfernen("-rv", "/github-liste.txt")
-        entfernen("-rv", "/updater.lua")
-        entfernen("-rv", "/json.lua")
+        for k, v in pairs({"/update", "/github-liste.txt", "/updater.lua", "/json.lua"}) do
+            entfernen("-rv", v)
+        end
         gpu.setForeground(0x00FF00)
-        print("Update vollständig")
-        os.sleep(5)
+        print("\nUpdate vollständig")
+        print("\nNeustart in 3s")
+        os.sleep(3)
         require("computer").shutdown(true)
     end
 end
@@ -155,19 +174,23 @@ end
 local function main()
     Funktion.checkKomponenten()
     gpu.setForeground(0xFFFFFF)
-    print("Starte Download\n")
-    if wget("-f", Funktion.Pfad("4") .. "Updater/json.lua", "/json.lua") then
-        if Funktion.verarbeiten() then
-            return
+    if hilfe then
+        Funktion.Hilfe()
+    else
+        print("Download Verzeichnisliste\n")
+        if wget("-f", Funktion.Pfad("1") .. "Updater/json.lua", "/json.lua") then
+            if Funktion.verarbeiten() then
+                return
+            end
         end
+        gpu.setForeground(0xFF0000)
+        print("<FEHLER> Download")
     end
-    gpu.setForeground(0xFF0000)
-    print("<FEHLER> GitHub Download")
 end
 
 if not pcall(main) then
     gpu.setForeground(0xFF0000)
-    print("<FEHLER> Funktion main")
+    print("<FEHLER> main")
 end
 
 gpu.setForeground(0xFFFFFF)
