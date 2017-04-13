4132dd33282b853d8f739c2c924457eac9772a6f
Nexarius <DarknessShadow@web.de>
2017-04-13 20:05:58 +0200

    Update Stargate-Adressen-suchen.lua

--- Stargate-Adressen-suchen.lua
+++ Stargate-Adressen-suchen.lua
@@ -1,12 +1,12 @@
 local sg = require("component").getPrimary("stargate")
 
-local nacheinander, zufall, check
-local A, B, C, D, E, F, G, H, I
+local Funktion = {}
+local A, B, C, D, E, F, G, H
 local alleZeichen = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
                      "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
                      "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
 
-local function nacheinander()
+function Funktion.nacheinander()
   for Ai = 1, 36 do
     A = alleZeichen[Ai]
     for Bi = 1, 36 do
@@ -24,8 +24,7 @@ local function nacheinander()
                 for Hi = 1, 36 do
                   H = alleZeichen[Hi]
                   for Ii = 1, 36 do
-                    I = alleZeichen[Ii]
-                    check({A, B, C, D, E, F, G, H, I})
+                    Funktion.check({A, B, C, D, "-", E, F, G, "-", H, alleZeichen[Ii]})
                   end
                 end
               end
@@ -37,17 +36,17 @@ local function nacheinander()
   end
 end
 
-local function zufall()
+function Funktion.zufall()
   while true do
     local adresse = {}
-    for i = 1, 7 do
+    for i = 1, 9 do
       adresse[i] = alleZeichen[math.random(1,36)]
     end
-    check(adresse)
+    Funktion.check(adresse)
   end
 end
 
-local function check(eingabe)
+function Funktion.check(eingabe)
   local Adresse = table.concat(eingabe)
   if sg.energyToDial(Adresse) then
     require("component").getPrimary("gpu").setForeground(0xFF0000)
@@ -66,10 +65,10 @@ local function main()
   while true do
     local eingabe = io.read()
     if tostring(eingabe) == "1" then
-      nacheinander()
+      Funktion.nacheinander()
       break
     elseif tostring(eingabe) == "2" then
-      zufall()
+      Funktion.zufall()
       break
     end
   end
