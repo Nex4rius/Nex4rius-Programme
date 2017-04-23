2f028642c26569f541b3474626e42ba941917010
Nexarius <DarknessShadow@web.de>
2017-04-23 16:34:54 +0200

    Update autorun.lua

--- Stargate-Programm/autorun.lua
+++ Stargate-Programm/autorun.lua
@@ -15,9 +15,12 @@ end
 if not pcall(loadfile("/stargate/check.lua"), args) then
     print("check.lua hat einen Fehler")
     os.sleep(2)
-    if loadfile("/bin/wget.lua")("-f", "https://raw.githubusercontent.com/Nex4rius/Nex4rius-Programme/master/GitHub-Downloader/github.lua", "/bin/github.lua") then
-        loadfile("/bin/github.lua")("Nex4rius", "Nex4rius-Programme", "master", "Stargate-Programm")
+    if loadfile("/bin/wget.lua")("-f", "https://raw.githubusercontent.com/Nex4rius/Nex4rius-Programme/master/Stargate-Programm/installieren.lua", "/installieren.lua") then
+        loadfile("/installieren.lua")()
     end
+    --if loadfile("/bin/wget.lua")("-f", "https://raw.githubusercontent.com/Nex4rius/Nex4rius-Programme/master/GitHub-Downloader/github.lua", "/bin/github.lua") then
+    --    loadfile("/bin/github.lua")("Nex4rius", "Nex4rius-Programme", "master", "Stargate-Programm")
+    --end
 end
 
 require("shell").setWorkingDirectory(alterPfad)
