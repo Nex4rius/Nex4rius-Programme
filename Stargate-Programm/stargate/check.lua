0b5cec8e94e327b9925ab8c16c40867f113d46c4
Nexarius <DarknessShadow@web.de>
2017-07-18 10:39:45 +0200

    Update check.lua

--- Stargate-Programm/stargate/check.lua
+++ Stargate-Programm/stargate/check.lua
@@ -37,12 +37,12 @@ elseif CC then
   gpu.fill = function(startX, startY, endX, endY) paintutils.drawFilledBox(startX, StartY, endX, endY, gpu.getBackgroundColor()) end
 end
 
-local kopieren = loadfile("/bin/cp.lua") or function(a, b)
-  if fs.exists(b) then
-    shell.run("delete " .. b)
-  end
-  shell.run(string.format("copy %s %s", a, b))
-end
+local kopieren = loadfile("/bin/cp.lua") or fs.copy --function(a, b)
+--  if fs.exists(b) then
+--   shell.run("delete " .. b)
+--  end
+--  shell.run(string.format("copy %s %s", a, b))
+--end
 local wget = loadfile("/bin/wget.lua") or function(option, url, ziel)
   if type(url) ~= "string" and type(ziel) ~= "string" then
     return
