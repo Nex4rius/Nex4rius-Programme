da2ef983a9c1cc0a7e746f0210f2c266ead83e6d
Nexarius <DarknessShadow@web.de>
2017-08-13 23:29:58 +0200

    Update Kontrollprogramm.lua

--- Stargate-Programm/stargate/Kontrollprogramm.lua
+++ Stargate-Programm/stargate/Kontrollprogramm.lua
@@ -1193,6 +1193,7 @@ function Taste.Pfeil_rechts()
     Funktion.zeigeHier(Taste.Koordinaten.Pfeil_rechts_X, Taste.Koordinaten.Pfeil_rechts_Y, "→ " .. sprachen.zeigeAdressen, 0)
   elseif seite == -2 then
     Funktion.zeigeHier(Taste.Koordinaten.Pfeil_rechts_X, Taste.Koordinaten.Pfeil_rechts_Y, "→ " .. sprachen.SteuerungName, 0)
+    Funktion.zeigeNachricht()
   elseif maxseiten > seite + 1 then
     Funktion.zeigeHier(Taste.Koordinaten.Pfeil_rechts_X, Taste.Koordinaten.Pfeil_rechts_Y, "→ " .. sprachen.naechsteSeite, 0)
   end
