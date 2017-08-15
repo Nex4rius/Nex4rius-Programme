ff336cc3003a3f3e3b23ae61a6ac005325557d9d
Nexarius <DarknessShadow@web.de>
2017-08-15 12:48:44 +0200

    Update Kontrollprogramm.lua

--- Stargate-Programm/stargate/Kontrollprogramm.lua
+++ Stargate-Programm/stargate/Kontrollprogramm.lua
@@ -464,21 +464,21 @@ function Funktion.AdressenSpeichern()
         sendeAdressen[i] = {}
         sendeAdressen[i][1] = na[1]
         sendeAdressen[i][2] = na[2]
-        if     anwahlEnergie <= 10000 then
+        if     anwahlEnergie < 10000 then
           anwahlEnergie = string.format("%.f" , (sg.energyToDial(na[2]) * energymultiplicator))
-        elseif anwahlEnergie > 10000 then
+        elseif anwahlEnergie < 10000000 then
           anwahlEnergie = string.format("%.1f", (sg.energyToDial(na[2]) * energymultiplicator) / 1000) .. " k"
-        elseif anwahlEnergie > 10000000 then
+        elseif anwahlEnergie < 10000000000 then
           anwahlEnergie = string.format("%.2f", (sg.energyToDial(na[2]) * energymultiplicator) / 1000000) .. " M"
-        elseif anwahlEnergie > 10000000000 then
+        elseif anwahlEnergie < 10000000000000 then
           anwahlEnergie = string.format("%.3f", (sg.energyToDial(na[2]) * energymultiplicator) / 1000000000) .. " G"
-        elseif anwahlEnergie > 10000000000000 then
+        elseif anwahlEnergie < 10000000000000000 then
           anwahlEnergie = string.format("%.3f", (sg.energyToDial(na[2]) * energymultiplicator) / 1000000000000) .. " T"
-        elseif anwahlEnergie > 10000000000000000 then
+        elseif anwahlEnergie < 10000000000000000000 then
           anwahlEnergie = string.format("%.3f", (sg.energyToDial(na[2]) * energymultiplicator) / 1000000000000000) .. " P"
-        elseif anwahlEnergie > 10000000000000000000 then
+        elseif anwahlEnergie < 10000000000000000000000 then
           anwahlEnergie = string.format("%.3f", (sg.energyToDial(na[2]) * energymultiplicator) / 1000000000000000000) .. " E"
-        elseif anwahlEnergie > 10000000000000000000000 then
+        elseif anwahlEnergie < 10000000000000000000000000 then
           anwahlEnergie = string.format("%.3f", (sg.energyToDial(na[2]) * energymultiplicator) / 1000000000000000000000) .. " Z"
         else
           anwahlEnergie = sprachen.zuvielEnergie
@@ -826,21 +826,21 @@ function Funktion.zeigeEnergie()
     Funktion.zeigeHier(xVerschiebung, zeile, "  " .. sprachen.energie1 .. energytype .. sprachen.energie2, 0)
     Funktion.SchreibInAndererFarben(xVerschiebung + string.len("  " .. sprachen.energie1 .. energytype .. sprachen.energie2), zeile, sprachen.keineEnergie, Farben.FehlerFarbe)
   else
-    if     energy <= 10000 then
+    if     energy < 10000 then
       energieMenge = string.format("%.f",  energy)
-    elseif energy > 10000 then
+    elseif energy < 10000000 then
       energieMenge = string.format("%.1f", energy / 1000) .. " k"
-    elseif energy > 10000000 then
+    elseif energy < 10000000000 then
       energieMenge = string.format("%.2f", energy / 1000000) .. " M"
-    elseif energy > 10000000000 then
+    elseif energy < 10000000000000 then
       energieMenge = string.format("%.3f", energy / 1000000000) .. " G"
-    elseif energy > 10000000000000 then
+    elseif energy < 10000000000000000 then
       energieMenge = string.format("%.3f", energy / 1000000000000) .. " T"
-    elseif energy > 10000000000000000 then
+    elseif energy < 10000000000000000000 then
       energieMenge = string.format("%.3f", energy / 1000000000000000) .. " P"
-    elseif energy > 10000000000000000000 then
+    elseif energy < 10000000000000000000000 then
       energieMenge = string.format("%.3f", energy / 1000000000000000000) .. " E"
-    elseif energy > 10000000000000000000000 then
+    elseif energy < 10000000000000000000000000 then
       energieMenge = string.format("%.3f", energy / 1000000000000000000000) .. " Z"
     else
       anwahlEnergie = sprachen.zuvielEnergie
