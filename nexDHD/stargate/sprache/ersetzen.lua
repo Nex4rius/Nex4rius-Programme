6f1099689e95ad49f672f2c34a5a507a8fbec0a6
Nexarius <DarknessShadow@web.de>
2017-08-16 19:47:34 +0200

    Create ersetzen.lua

---
+++ nexDHD/stargate/sprache/ersetzen.lua
@@ -0,0 +1,21 @@
+-- pastebin run -f YVqKFnsP
+-- nexDHD von Nex4rius
+-- https://github.com/Nex4rius/Nex4rius-Programme/tree/master/nexDHD
+
+local sprachen  = ...
+
+return {
+  ["On"]              = sprachen.irisKontrolleNameAn,
+  ["Off"]             = sprachen.irisKontrolleNameAus,
+  ["Open"]            = sprachen.irisNameOffen,
+  ["Opening"]         = sprachen.irisNameOeffnend,
+  ["Closed"]          = sprachen.irisNameGeschlossen,
+  ["Closing"]         = sprachen.irisNameSchliessend,
+  ["Offline"]         = sprachen.irisNameOffline,
+  ["Override"]        = sprachen.Eingriff,
+  ["Manual"]          = sprachen.manueller,
+  ["Control"]         = sprachen.IrisSteuerungName,
+  ["Accepted"]        = sprachen.akzeptiert,
+  ["Atmogood"]        = sprachen.atmosphere2 .. sprachen.atmosphereJA,
+  ["Atmodangerous"]   = sprachen.atmosphere2 .. sprachen.atmosphereNEIN,
+}
