49b78c48d9b7c726867efba569fb40b9ff0d454b
Nexarius <DarknessShadow@web.de>
2017-08-16 19:45:36 +0200

    Create adressen.lua

---
+++ nexDHD/stargate/adressen.lua
@@ -0,0 +1,17 @@
+-- pastebin run -f YVqKFnsP
+-- nexDHD von Nex4rius
+-- https://github.com/Nex4rius/Nex4rius-Programme/tree/master/nexDHD
+--
+-- to save press "Ctrl + S"
+-- to close press "Ctrl + W"
+--
+-- Put your own stargate addresses here
+-- "" for no Iris Code
+--
+
+return {
+--{"<Name>", "<Adresse>", "<IDC>"},
+  {"Name 1", "XXXX-XXX-XX", ""},
+  {"Name 2", "XXXX-XXX-XX", ""},
+  {"Name 3", "XXXX-XXX-XX", ""},
+}
