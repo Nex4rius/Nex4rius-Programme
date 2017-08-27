b472c8ad0981012d9aa75e82ab8ec9be3ce65c9b
Nexarius <github@nexarius.xyz>
2017-08-27 23:39:24 +0200

    Update anzeige.lua

--- Tank/server/tank/anzeige.lua
+++ Tank/server/tank/anzeige.lua
@@ -430,7 +430,7 @@ function f.senden()
 end
 
 function f.test(screenid)
-  f.test = function() end
+  return --deaktiviert
   os.sleep(0.1)
   local screenid = screenid or gpu.getScreen()
   local _, hoch = component.proxy(screenid).getAspectRatio()
