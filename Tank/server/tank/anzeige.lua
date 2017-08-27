b7739a819074a99b49899618f6a9e32cc60c7e96
Nexarius <github@nexarius.xyz>
2017-08-27 15:32:17 +0200

    Update anzeige.lua

--- Tank/server/tank/anzeige.lua
+++ Tank/server/tank/anzeige.lua
@@ -446,9 +446,12 @@ local function test(screenid)
   for _, farbe in pairs(hex) do
     gpu.setBackground(farbe)
     print(string.rep(" ", 160))
-    os.sleep(0.1)
+    print(string.rep(" ", 160))
+    os.sleep(0.01)
   end
-  gpu.setBackground(0x000000)
+  for i = 0, 80 do
+    print(string.rep(" ", 160))
+  end
 end
 
 function f.checkUpdate()
