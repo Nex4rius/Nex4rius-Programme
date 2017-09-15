74f3d1c5c5bf07bf941dd2e80f954fbb09572bb9
Nexarius <github@nexarius.xyz>
2017-09-15 11:35:07 +0200

    Update debug.lua

--- Tank/server/tank/debug.lua
+++ Tank/server/tank/debug.lua
@@ -1,1 +1,15 @@
 local component = require("component")
+local event = require("event")
+local i = 0
+
+local d = io.open("/log", "w")
+local function lesen(i, ...)
+  d:write(i .. ...)
+  print(i .. ...)
+end
+
+while true do
+  i = i + 1
+  lesen(i, event.pull())
+end
+d:close()
