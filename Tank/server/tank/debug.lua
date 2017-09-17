28021c2c2b2ece0caf69655e6ff679c94378c102
Nexarius <github@nexarius.xyz>
2017-09-17 09:28:33 +0200

    Update debug.lua

--- Tank/server/tank/debug.lua
+++ Tank/server/tank/debug.lua
@@ -1,17 +1,22 @@
 local component = require("component")
 local event = require("event")
+local term = require("term")
 local i = 0
 
 component.getPrimary("modem").open(100)
 
 local d = io.open("/log", "w")
-local function lesen(i, ...)
-  d:write(i .. ...)
-  print(i .. ...)
+local function lesen(...)
+  for k, v in pairs({...}) do
+    d:write(tostring(v) .. "   ")
+    term.write(tostring(v) .. "   ")
+  end
+  print()
 end
 
 while true do
   i = i + 1
-  lesen(i, event.pull())
+  local a, b, c, d, e, f, g, h = event.pull()
+  lesen(i, a, b, c, d, e, f, g, h)
 end
 d:close()
