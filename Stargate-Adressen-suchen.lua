bba873f1234eb29e9164f9fc086f23a27b712045
Nexarius <github@nexarius.xyz>
2017-09-19 14:34:26 +0200

    Update Stargate-Adressen-suchen.lua

--- Stargate-Adressen-suchen.lua
+++ Stargate-Adressen-suchen.lua
@@ -1,12 +1,13 @@
 local sg = require("component").getPrimary("stargate")
 
-local Funktion = {}
+local gefunden = 0
+local f = {}
 local A, B, C, D, E, F, G, H
 local alleZeichen = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
                      "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
                      "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
 
-function Funktion.nacheinander()
+function f.nacheinander()
   for Ai = 1, 36 do
     A = alleZeichen[Ai]
     for Bi = 1, 36 do
@@ -24,7 +25,7 @@ function Funktion.nacheinander()
                 for Hi = 1, 36 do
                   H = alleZeichen[Hi]
                   for Ii = 1, 36 do
-                    Funktion.check({A, B, C, D, "-", E, F, G, "-", H, alleZeichen[Ii]})
+                    f.check({A, B, C, D, "-", E, F, G, "-", H, alleZeichen[Ii]})
                   end
                 end
               end
@@ -36,41 +37,38 @@ function Funktion.nacheinander()
   end
 end
 
-function Funktion.zufall()
+function f.zufall()
   while true do
-    local adresse = {}
-    for i = 1, 9 do
-      adresse[i] = alleZeichen[math.random(1,36)]
-    end
-    Funktion.check(adresse)
+    f.check(alleZeichen[math.random(1,36)], alleZeichen[math.random(1,36)], alleZeichen[math.random(1,36)], alleZeichen[math.random(1,36)], "-", alleZeichen[math.random(1,36)], alleZeichen[math.random(1,36)], alleZeichen[math.random(1,36)], "-", alleZeichen[math.random(1,36)], alleZeichen[math.random(1,36)])
   end
 end
 
-function Funktion.check(eingabe)
+function f.check(eingabe)
   local Adresse = table.concat(eingabe)
   if sg.energyToDial(Adresse) then
-    require("component").getPrimary("gpu").setForeground(0xFF0000)
-    require("component").getPrimary("gpu").setBackground(0x006633)
+    local gpu = require("component").getPrimary("gpu")
+    gpu.setForeground(0xFF0000)
+    gpu.setBackground(0x006633)
     print("\n" .. Adresse .. " GEFUNDEN\n")
-    f = io.open("ergebnis_suche", "a")
-    f:write(Adresse .. "\n")
-    f:close ()
-    require("component").getPrimary("gpu").setForeground(0xFFFFFF)
-    io.read()
+    local d = io.open("ergebnis_suche", "a")
+    d:write(Adresse .. "\n")
+    d:close ()
+    gpu.setForeground(0xFFFFFF)
+    gefunden = gefunden + 1
   else
-    print(Adresse)
+    print("Prüfe: ", Adresse, "Gefunden: ", gefunden)
   end
 end
 
 local function main()
-  print("1)Nacheinander oder 2)Zufall?")
+  print("1) Nacheinander oder 2) Zufall?")
   while true do
     local eingabe = io.read()
     if tostring(eingabe) == "1" then
-      Funktion.nacheinander()
+      f.nacheinander()
       break
     elseif tostring(eingabe) == "2" then
-      Funktion.zufall()
+      f.zufall()
       break
     end
   end
