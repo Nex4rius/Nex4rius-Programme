8ce251b4794c612da430b1ccd99a55833782bd29
Nexarius <github@nexarius.xyz>
2017-11-25 15:38:22 +0100

    Update anzeige.lua

--- Tank/server/tank/anzeige.lua
+++ Tank/server/tank/anzeige.lua
@@ -452,15 +452,13 @@ function f.datei(id, datei)
         printwlan("datei start") --debug
         local i = 0
         local art = "w"
-        local max_packet = 1000
+        local max_packet = 8000
         gpu.setResolution(gpu.maxResolution())
         while true do
             local sende_inhalt = string.sub(inhalt, max_packet * i + 1, max_packet * (i + 1))
-            printwlan(max_packet * i + 1) -- debug
-            printwlan(max_packet * (i + 1)) -- debug
-            if sende_inhalt == "" then break end
-            printwlan("art: " .. art .. "\nloop " .. i .. "\nlänge" .. string.len(sende_inhalt)) -- debug
-            print(sende_inhalt) -- debug
+            if sende_inhalt == "" then
+                break
+            end
             m.send(id, port, "datei", datei, sende_inhalt, art)
             i = i + 1
             art = "a"
