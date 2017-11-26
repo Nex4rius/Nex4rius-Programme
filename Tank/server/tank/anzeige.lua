dbee53eab6aa55c4e4552b5ac703eaff664bf537
Nexarius <github@nexarius.xyz>
2017-11-26 15:18:42 +0100

    Update anzeige.lua

--- Tank/server/tank/anzeige.lua
+++ Tank/server/tank/anzeige.lua
@@ -70,6 +70,8 @@ m.setStrength(math.huge)
 printwlan("start")
 
 function f.tank(hier, id, nachricht)
+    printwlan("nachricht", serialization.serialize(nachricht))
+    printwlan("tank1", serialization.serialize(tank))
     local dazu = true
     local ende = 0
     if hier then
@@ -132,7 +134,7 @@ function f.verarbeiten(tank)
                     local weiter = true
                     if name ~= "Tankname" then
                         for k in pairs(tank_a[gruppe]) do
-                            if tank_a[gruppe].name == name then
+                            if tank_a[gruppe][k].name == name then
                                 tank_a[gruppe][k].menge = tank_a[gruppe][k].menge + menge
                                 tank_a[gruppe][k].maxmenge = tank_a[gruppe][k].maxmenge + maxmenge
                                 weiter = false
@@ -154,7 +156,14 @@ function f.verarbeiten(tank)
     tankneu = {}
     for gruppe, v in pairs(tank_a) do
         for _, w in pairs(v) do
-            tankneu[#tankneu + 1] = w
+            if w.name == "Tankname" then
+                tankneu[#tankneu + 1] = w
+            end
+        end
+        for _, w in spairs(v, function(t,a,b) return tonumber(t[b].menge) < tonumber(t[a].menge) end) do
+            if w.name ~= "Tankname" then
+                tankneu[#tankneu + 1] = w
+            end
         end
     end
     printwlan("tank_a", serialization.serialize(tank_a), "\n\ntankneu", serialization.serialize(tankneu))
