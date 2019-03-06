3b84646053aaadc83456c536ee9a6ece7c3ae2cb
Nexarius <github@nexarius.xyz>
2019-03-06 21:14:48 +0100

    Update Kontrollprogramm.lua

--- nexDHD/stargate/Kontrollprogramm.lua
+++ nexDHD/stargate/Kontrollprogramm.lua
@@ -1686,9 +1686,7 @@ function f.check_IDC(code)
     v.IDC_Anzahl = v.IDC_Anzahl + 1
     if direction == "Incoming" and code ~= "Adressliste" then
       incode = code
-      if incode == Sicherung.IDC then
-        IDCyes = true
-      end
+      f.Iriskontrolle()
     end
   else
     sg.sendMessage(sprachen.IDC_blockiert)
