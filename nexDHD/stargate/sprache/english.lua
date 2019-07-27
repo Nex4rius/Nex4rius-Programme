f49d0e16ca3f42555753c195e8e18bb879c6a590
Nexarius <github@nexarius.xyz>
2019-07-28 01:11:37 +0200

    Update english.lua

--- nexDHD/stargate/sprache/english.lua
+++ nexDHD/stargate/sprache/english.lua
@@ -167,6 +167,7 @@ local sprachen = {
   anwahl_fehler             = "unknown error at dialling",
   cloud                     = "up- and download of addresses into the cloud",
   cloud_arbeit              = "The addresses are being synced with the cloud.",
+  cloud_fertig              = "syncing finished",
 }
 
 sprachen.side               = string.format("%s, %s, %s, %s, %s %s %s", sprachen.unten, sprachen.oben, sprachen.hinten, sprachen.vorne, sprachen.rechts, sprachen.oder, sprachen.links)
